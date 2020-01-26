366523c7c8b87a37293eb107a3943f6d9c3459f8
p1gc0rn <pennyle0914@gmail.com>
2020-01-26 15:31:41 +1100

    add home page

--- react-website/src/aboutUs.js
+++ react-website/src/aboutUs.js
@@ -3,7 +3,38 @@ import React, { Component } from 'react'
 class AboutUs extends Component{
 	render(){
 		return (
-			<h1> About Us </h1>
+			<div>
+			 <div class="container">
+                		<img src={process.env.PUBLIC_URL + '/arts-build-close-up-commerce-273230.jpg'} className="" alt="banner" resizeMode='contain'
+                		style={{height: 768,width: 1366}} />
+				<div class="headline">
+					<h1>UNSW</h1>
+					<h1>Women In Techonology</h1>
+					<p class="title">
+						Empowering and Inspiring 
+						<br/>
+						the Artchitects of Change
+					</p>
+				</div>		
+				<button class="button btn">Join Us</button>
+			</div>
+			<div class="unite">	
+		
+				<h1>Unite, Empower & Upskill </h1>
+				<p>
+				Technology is the driving force of our future and is a vital part of our everyday lives, and yet the industry still does not represent the society that they’re creating solutions for. That’s why we are passionate about equality and diversity in the tech industry.
+
+Formed in late 2016, we have grown to be a platform that empowers, unites and up-skills female and male students alike that are passionate about our mission. Women in Technology (WIT) aims to cultivate future leaders that are prepared  to challenge the prejudices and bring change into the industry while building a strong community where they can find lasting friendships and support.
+
+At WIT, our focus is on providing events that foster development catering to first to final years. Whether it’s personal, academic, professional or social, we want our students to be the best version of themselves. As part out aim, we also connect current and future students with alumni and industry sponsors to serve as role models to inspire.
+
+In 2020, we aim to go above and beyond to create events and opportunities for our members to learn and develop both their technical and professional skills.
+				</p>
+			</div>
+			 <img src={process.env.PUBLIC_URL + '/Untitled-1.jpg'} className="" alt="banner" resizeMode='contain'
+                                style={{width: '100vw'}} />
+
+			</div>	
 		
 		);
 
--- react-website/src/contactUs.js
+++ react-website/src/contactUs.js
@@ -1,13 +1,77 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class ContactUs extends Component{
-	render(){
-		return (
-			<h1> Contact Us </h1>
-		
-		);
-
-	}
+class ContactUs extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1 className='centre'>Contact Us</h1>
+                <div class='row'> 
+                    <div class='column' className='column' style={{float:"left", width: '20%'}}>
+                        <p className='subheader' style={{textAlign: 'center'}}>Send Through a General Inquiry</p>
+                        <form action="mailto:someone@example.com" method="post" enctype="text/plain" style={{width : "35%"}}>
+                            Name
+                            <br/>
+                            <input type="text" name="name" />
+                            <br/>
+                            E-mail
+                            <br/>
+                            <input type="text" name="mail" />
+                            <br/>
+                            Message
+                            <br/>
+                            <textarea name="message" rows="5"></textarea>
+                            <br/>
+                            <input type="submit" value="Send" />
+                        </form>
+                    </div>
+                    <div class='column' className='column' style={{float: 'left', width : '80%'}}>
+                        <p className='subheader' style={{textAlign: 'center'}}>Contact Our Team</p>
+                        <div class='row'> 
+                            <div class='column' className ='column' style={{float: 'left', width : '15%', textAlign: 'right'}}>
+                                <p>Vivian Shen</p>
+                                <p>Amanda Li</p>
+                                <p>Kelly Liang</p>
+                                <p>Elizabeth Zhong</p>
+                                <br/>
+                                <p>Vivian Wong</p>
+                                <p>Gabrielle Younes</p>
+                                <p>Rhea Thomas</p>
+                                <p>Alison Chin</p>
+                                <p>Elicia Au Duong</p>
+                                <p>Felicia Ee</p>  
+                            </div>
+                            <div class='column' className ='column' style={{float: 'left', width : '15%'}}>
+                                <p>president@unswwit.com</p>
+                                <p>vp@unswwit.com</p>
+                                <p>secretary@unswwit.com</p>
+                                <p>treasurer@unswwit.com</p>
+                                <br />
+                                <p>vivianwong@unswwit.com</p>
+                                <p>gabriella@unswwit.com</p>
+                                <p>rhea@unswwit.com</p>
+                                <p>alison@unswwit.com</p>
+                                <p>elicia@unswwit.com</p>
+                                <p>felicia@unswwit.com</p> 
+                            </div>
+                            <div class='column' className = 'column' style={{float: 'left', width : '15%'}}>
+                                <p></p> <br/>
+                                <p></p> <br/>
+                                <p></p> <br/>
+                                <p></p> <br/>
+                                <br/>
+                                <p>education@unswwit.com</p>
+                                <p>events@unswwit.com</p>
+                                <p>externals@unswwit.com</p>
+                                <p>hr@unswwit.com</p>
+                                <p>it@unswwit.com</p>
+                                <p>marketing@unswwit.com</p> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default ContactUs
