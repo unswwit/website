7ef039fc508183c834942485e8f112e38290b615
Elicia Au Duong <eliciaauduong@outlook.com>
2020-02-25 20:48:02 +1100

    clean code for homepage

--- src/aboutUs.js
+++ src/aboutUs.js
@@ -8,8 +8,8 @@ class AboutUs extends Component{
 			<body>
 
 			<div class="contain">
-				<img src={process.env.PUBLIC_URL + '/arts-build-close-up-commerce-273230.jpg'} class="banner" alt="banner"
-					style={{maxHeight: 6000}}/>
+				<img src={process.env.PUBLIC_URL + '/header.jpg'} class="banner" alt="header"
+					style={{maxHeight: 5000}}/>
 
 				<div class="headline">
 					<h1>UNSW</h1>
@@ -25,20 +25,29 @@ class AboutUs extends Component{
 
 			<div class="unite">
 				<h1 style={{padding: '0px 0px 50px 0px'}}>Unite, Empower & Upskill</h1>
-				<p>
-				Technology is the driving force of our future and is a vital part of our everyday lives, and yet the industry still does not represent the society that they’re creating solutions for. That’s why we are passionate about equality and diversity in the tech industry.
-				</p> <p>Formed in late 2016, we have grown to be a platform that empowers, unites and up-skills female and male students alike that are passionate about our mission. Women in Technology (WIT) aims to cultivate future leaders that are prepared  to challenge the prejudices and bring change into the industry while building a strong community where they can find lasting friendships and support.
-
-At WIT, our focus is on providing events that foster development catering to first to final years. Whether it’s personal, academic, professional or social, we want our students to be the best version of themselves. As part out aim, we also connect current and future students with alumni and industry sponsors to serve as role models to inspire.
-
-</p><p>In 2020, we aim to go above and beyond to create events and opportunities for our members to learn and develop both their technical and professional skills.
-
+				<p class="description">
+					Technology is the driving force of our future and is a vital part of our everyday lives, 
+					and yet the industry still does not represent the society that they’re creating solutions for. 
+					That’s why we are passionate about equality and diversity in the tech industry.</p> 
+				<p class="description">
+					Formed in late 2016, we have grown to be a platform that empowers, unites and up-skills female 
+					and male students alike that are passionate about our mission. Women in Technology (WIT) 
+					aims to cultivate future leaders that are prepared to challenge the prejudices and bring 
+					change into the industry while building a strong community where they can find lasting friendships and support.</p>
+				<p class="description">
+					At WIT, our focus is on providing events that foster development catering to first to final years. 
+					Whether it’s personal, academic, professional or social, we want our students to be the best version of themselves. 
+					As part out aim, we also connect current and future students with alumni and industry sponsors to serve as role models to inspire.</p>
+				<p class="description">
+					In 2020, we aim to go above and beyond to create events and opportunities for our members to learn and develop both their technical and professional skills.
 				</p>
 			</div>
 			<div class="row" style={{'padding': '20px 100px 100px '}}>
 				<div class="col-md-4">
 					<h2>Upcoming Events</h2>
-					<p style={{'font-size': '20px'}}>It's never too late to get involved with WIT and upskill your technical and soft skills!</p>
+					<p style={{'font-size': '18px'}}>
+						It's never too late to get involved with WIT and upskill your technical and soft skills!
+					</p>
 					<img src={process.env.PUBLIC_URL + '/app-scratch.png'} className="event1" alt="banner"  />
 				</div>
 				<div class="col-lg-8">
@@ -48,7 +57,7 @@ At WIT, our focus is on providing events that foster development catering to fir
 
 			<div class="stats" onScroll={this.handleScroll}>
 				
-				<img src={process.env.PUBLIC_URL + './Untitled-2.1.jpg'} className="photo-1" alt="banner" resizeMode='contain'
+				<img src={process.env.PUBLIC_URL + './metrics-bg.jpg'} className="photo-1" alt="banner" resizeMode='contain'
                 		style={{width: '100vw'}}/>
 				<div class="number">
 					<span><CountUp end={10} duration={4}/></span>
@@ -57,7 +66,7 @@ At WIT, our focus is on providing events that foster development catering to fir
 				</div>
 				<div class="words">
 					<span>Team Members</span>
-					<span>Sponsors </span>
+					<span>Sponsors</span>
 					<span>Members</span>
 				</div>
 
@@ -68,7 +77,7 @@ At WIT, our focus is on providing events that foster development catering to fir
 						Collaborate • Inspire • Change
 					</p>
 					
-					<p>
+					<p style={{'font-size': '14px'}}>
 						Join UNSW WIT via SpARC <br/>
 					</p>
 					<button class="button btn-sparc">Join Us</button>
