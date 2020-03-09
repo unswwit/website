9bbccf420a3d7b35111ccdb7fce621a183074717
Elicia Au Duong <eliciaauduong@outlook.com>
2020-03-09 11:30:38 +1100

    final build before subcom

---
+++ build/precache-manifest.dbc549e30668a5ed27136a8aa373ef04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a238eb342a47ab4ac282c7c19899612",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62441daaa44cbec957ca",
+    "url": "/static/css/2.ff4cff7f.chunk.css"
+  },
+  {
+    "revision": "bf5f7022e2504d2f864b",
+    "url": "/static/css/main.6e0d6ea6.chunk.css"
+  },
+  {
+    "revision": "62441daaa44cbec957ca",
+    "url": "/static/js/2.dfa7cd95.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.dfa7cd95.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bf5f7022e2504d2f864b",
+    "url": "/static/js/main.e424b98a.chunk.js"
+  },
+  {
+    "revision": "3bcb6824372899d729c8",
+    "url": "/static/js/runtime-main.f32eaec2.js"
+  }
+]);
