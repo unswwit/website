d486f3500061c12c2fd7aaa5b365b8bfbddbca26
chelffey <chaffeychelsea@gmail.com>
2020-03-23 16:25:23 +1100

    layout for joinUs webpage + static map

--- src/joinUs.js
+++ src/joinUs.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import "./joinUs.css"
+
 class JoinUs extends React.Component {
     render() {
         const spARCUrl = "https://member.arc.unsw.edu.au/s/clubdetail?clubid=0016F0000371W0xQAE";
@@ -9,50 +11,38 @@ class JoinUs extends React.Component {
         const instagramUrl = "https://www.instagram.com/wit.unsw/";
 
         return (
+
             <div>
-                <img src={process.env.PUBLIC_URL + '/wit_cover.jpg'} className="centre" alt="banner" 
-                resizeMode='contain'
-                style={{
-                    maxHeight: 768,
-                    maxWidth: 1366,
-                    }} />
-                <h1 className='centre'>Join Us!</h1>
-                <p className='subheader centre'>{`
-                        How you can get involved
-                    `}
-                </p>
-                <div className='joinColumn'>
-                    <p className='subheader'>{`
-                        Become an official Member by joining us on SpARC
-                    `}
-                    </p>
-                    <a className='button' href={spARCUrl}>
-                        SpARC
+              <div className = 'joinUsHeader'>
+                <img src={process.env.PUBLIC_URL + '/join-header.png'} width = "100%" className="centre" alt="banner"
+                resizeMode='cover'
+                />
+                <h1 className='centre'>Join Us</h1>
+              </div>
+
+              <div className = 'joinUsBody'>
+                <img src={process.env.PUBLIC_URL + '/join-us-map.png'} width = "100%" className="googleMap" alt="map"
+                />
+                <div className = 'joinUsRight'>
+                  <h2>Connect with us</h2>
+                  <div className = 'joinButtons'>
+                    <a href={fbPageUrl}> FB
                     </a>
-                    <p className='subheader'>{`
-                        Keep updated by following us on Facebook
-                    `}
-                    </p>
-                    <a className='button' href={fbPageUrl}>
-                        FB Page
+                    <a href={instagramUrl}>
+                        Insta
                     </a>
-                    <div className='divider'></div>
-                    <a className='button' href={fbGroupUrl}>
-                        FB Group
-                    </a> 
-                    <p className='subheader'>{`
-                        Follow us on Other Social Media Accounts 
-                    `}
-                    </p>
-                    <a className='button' href={linkedInUrl}>
-                        LinkedIn
+                    <a href={linkedInUrl}>
+                        Linked
                     </a>
-                    <div className='divider'></div>
-                    <a className='button' href={instagramUrl}>
-                        Instagram
-                    </a>
+                  </div>
+                  <h2>Subscribe to our mailing list</h2>
+                  <div className = 'mailingForm' >
+                  </div>
                 </div>
+              </div>
+
             </div>
+
         );
     }
 }
