a066fbbbe788d4fa9c3cf43879ff3c835cb15b02
Elicia Au Duong <eliciaauduong@outlook.com>
2020-03-30 00:28:34 +1100

    updated sponsors contact email formatting

--- src/sponsors.js
+++ src/sponsors.js
@@ -180,7 +180,7 @@ class Sponsors extends React.Component {
                 </a>
     
                 <p className='subheader' style={{'font-size': '16px'}}>{`Interested in partnering with us? Contact us at `}
-                    <a className='subheader' style={{'font-size': '16px'}} href="mailto:externals@unswwit.com">externals@unswwit.com</a>.
+                    <a class='subheader' style={{'font-size': '16px'}} href="mailto:externals@unswwit.com">externals@unswwit.com</a>
                 </p>
             </div>
         );
