74c78568f1fe5c34121c2ae8a736f1eeec0a53af
chelffey <chaffeychelsea@gmail.com>
2020-05-30 22:34:02 +1000

    added publications page link to footer

--- src/footer.js
+++ src/footer.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import "./style.css";
 class Footer extends Component{
-    
+
 	render(){
 		return (
             <div>
                 <footer>
                     <ul class="footer-links">
-                        <li class="footer-item"><img src={process.env.PUBLIC_URL + './logo-black.png'} className="photo" alt="wit logo" 
+                        <li class="footer-item"><img src={process.env.PUBLIC_URL + './logo-black.png'} className="photo" alt="wit logo"
                             resizeMode='contain' style={{width: '50px'}} /></li>
                         <li class="footer-item"><a href="/">Home</a></li>
                         <li class="footer-item"><a href="/our-team">Team </a></li>
@@ -15,6 +15,7 @@ class Footer extends Component{
                         <li class="footer-item"><a href="/blog-posts">Blog Posts </a></li>
                         <li class="footer-item"><a href="/join-us">Join </a></li>
                         <li class="footer-item"><a href="/contact-us">Contact</a></li>
+                        <li class="footer-item"><a href="/publications">Publications</a></li>
                         <li class="footer-item"><a href="https://www.linkedin.com/company/unsw-women-in-technology"><img src={process.env.PUBLIC_URL + '/linkedin.png'} className="" alt="banner" resizeMode='contain'
                             style={{width:'25px', height:'25px'}} /></a></li>
                         <li class="footer-item"><a href="https://www.facebook.com/unsw.wit/"><img src={process.env.PUBLIC_URL + '/facebook.png'} className="" alt="banner" resizeMode='contain'
@@ -25,7 +26,7 @@ class Footer extends Component{
                     <div class="footer-copyright text-center">Copyright © 2020 | <li class="footer-item" id="copyright"><a href="https://unswwit.com/">UNSW Women in Technology</a></li></div>
                 </footer>
             </div>
-            
+
 		);
 
 	}
