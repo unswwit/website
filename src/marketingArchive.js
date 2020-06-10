dbc4dcccd3bc54b10d0621536ed3b8ffb85bd873
Lily Yang <lilyyang@Lilys-MacBook-Pro-2.local>
2020-06-10 22:08:41 +1000

    marketing archive part 1

---
+++ src/marketingArchive.js
@@ -0,0 +1,33 @@
+//All necessary imports for this javascript
+import React, { Component } from "react";
+import "./style.css";
+import "./marketingArchive.css";
+import { Link } from "react-router-dom";
+
+class MarketingArchive extends Component {
+  render() {
+    return (
+        <div>
+
+            {/*Start of Header*/}
+            <div class="marketingCoverPhoto">
+                <div class="title">
+                    <h1>Marketing Archive</h1>
+                </div>
+            </div>
+            {/*End of Header*/}
+
+            {/*Start of text*/}
+            <div class="introParagraph">
+              <p class="intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur molestie eu tellus sed facilisis. Aliquam eget interdum ipsum. Vivamus rutrum sapien velit, eget finibus enim pellentesque at. Ut sed felis nisi. Maecenas.</p>
+            </div>
+
+            {/*2020 section*/}
+            <div class="yearSection">
+              <p class="year">2020</p>
+            </div>
+        </div>
+    );
+  }
+}
+export default MarketingArchive;
