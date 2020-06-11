1423a23cbe8d4c1b04f5de8a805773f6b5792518
Lily Yang <lilyyang@Lilys-MacBook-Pro-2.local>
2020-06-11 11:19:27 +1000

    completed part 1

--- src/marketingArchive.js
+++ src/marketingArchive.js
@@ -2,7 +2,6 @@
 import React, { Component } from "react";
 import "./style.css";
 import "./marketingArchive.css";
-import { Link } from "react-router-dom";
 
 class MarketingArchive extends Component {
   render() {
