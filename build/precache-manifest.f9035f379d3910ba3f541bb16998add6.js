fd1d8e40f36fea09bd2df61596967de076361258
Elicia Au Duong <eliciaauduong@outlook.com>
2020-06-21 12:38:54 +1000

    blog post 7 release

--- build/precache-manifest.f9035f379d3910ba3f541bb16998add6.js
+++ build/precache-manifest.f9035f379d3910ba3f541bb16998add6.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2e982bfd56eebb8ffb6cf124deb716ee",
+    "revision": "86951e965c9d02ef115ed638b7076824",
     "url": "/index.html"
   },
   {
-    "revision": "016b8af7366ae03288ba",
+    "revision": "1947466a63b2649559ab",
     "url": "/static/css/2.47e06e2e.chunk.css"
   },
   {
-    "revision": "3105fd1994aebf63de0f",
+    "revision": "4c3ee26d8cce22c7b1ce",
     "url": "/static/css/main.fad3f1e0.chunk.css"
   },
   {
-    "revision": "016b8af7366ae03288ba",
-    "url": "/static/js/2.9ed8bcf8.chunk.js"
+    "revision": "1947466a63b2649559ab",
+    "url": "/static/js/2.4e195a90.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/static/js/2.9ed8bcf8.chunk.js.LICENSE"
+    "url": "/static/js/2.4e195a90.chunk.js.LICENSE"
   },
   {
-    "revision": "3105fd1994aebf63de0f",
-    "url": "/static/js/main.1b192fe6.chunk.js"
+    "revision": "4c3ee26d8cce22c7b1ce",
+    "url": "/static/js/main.521854d2.chunk.js"
   },
   {
     "revision": "3bcb6824372899d729c8",
