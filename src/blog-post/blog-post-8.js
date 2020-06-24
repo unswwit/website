0b2d3152b4b0b8b0c037f9b7bd0564dd1471f105
chelffey <chaffeychelsea@gmail.com>
2020-06-24 14:46:50 +1000

    uploaded blog post

--- src/blog-post/blog-post-8.js
+++ src/blog-post/blog-post-8.js
@@ -18,56 +18,84 @@ class blogPost8 extends Component {
               {/*Start of blog post*/}
               <div class="blog-content">
                 <div>
-                  <h2 class = "blog-title">Introducing: Fontaine Foxworth, Product Manager of Google</h2>
-                  <p class = "date"> 17th June 2020 </p>
+                  <h2 class = "blog-title">WIT Crush Wednesday - Dr Marguerite Evans-Galea</h2>
+                  <p class = "date"> 24th June 2020 </p>
                 </div>
 
                 <div class="post">
 
-                  <p class="post-content">Despite studying Civil Engineering at Stanford University, Fontaine Foxworth discovered her true passions
-                  within computer-science and technology, and is now a Mobile Product manager. She is experienced at facilitating projects across
-                  all stages of development, previously working as a Product Manager at ModCloth (an online clothing retailer) where she governed
-                  the design and production of the company’s mobile web and apps. Foxworth is currently a product-manager at Google, where she
-                  worked on Google Analytics, Maps API, and Blogger.</p><br/>
+                  <p class="post-content">Dr. Marguerite Evans-Galea is one of Australia’s leading
+                  scientists, respected and known internationally for her research in cell and
+                  gene therapies.</p><br/>
 
-                  <p>Let’s read what Fontaine had to say about her journey into tech below</p>
+                  <p class="post-content">In a 2017 radio interview published by ABC Sunday Extra in 2017, Marguerite
+                  reflected on her path into STEM and the challenges women faced entering these
+                  professions. But STEM was not her first choice. Initially, Marguerite was drawn
+                  to classical music and dreamed of becoming a music therapist. Whilst reading the
+                  discovery of the structure of DNA by James Watson, Marguerite fell in love with DNA
+                   in high school. She expressed that “I loved the idea of solving something at the
+                   molecular level”, and was immediately drawn to the complexities and excited by
+                   the challenges in science.</p><br/>
 
-                  <ol class="list">
-                    
-                    <li class="question">Where do you see yourself in the future (career wise)?</li>
-                    <p class="post-content">My big passion in life is teaching people about EMOTIONS! I think I have a unique skill set in that I have
-                    both deep technical understanding and an infinite passion for emotions. I want to bring those two together, and ultimately help people
-                    better understand themselves. Combining my love for emotions and technology could go in a million directions, but I'm hoping my next
-                    role can either be around Mental Health or Ethics & Technology.</p><br/>
 
-                    <li class="question"> What do you love about your career now?</li>
-                    <p class="post-content">As a product manager for a blogging platform, I love that I help people express themselves. I care deeply about
-                    self-discovery and expression, and blogging is a great way for people to better understand themselves.</p><br/>
+                  <p class="post-content">Graduating with a Ph.D. in Molecular Biology from UNSW, Marguerite was
+                  enthusiastic to start her career in the US as a postdoctoral fellow at the University of Utah.
+                  Here, she really began to understand the premise of a male-dominant culture in STEM. As a
+                  leading advocate for women in technology, Margarite does not believe there is any lack of
+                  interest, passion, or ambition for girls and women to enter the STEM field. However, she
+                  outlines that the field has not been the most accommodating to women, especially for those
+                  who want families. She finds that the rolling contracts and competitive atmosphere in STEM
+                  are unforgiving for women who take career breaks to have a child, posing as a significant barrier
+                  for women interested in STEM. Nonetheless, she managed to successfully build a family whilst
+                  growing her career! </p><br/>
 
-                    <li class="question"> Favourite Quote or Motto?</li>
-                    <p class="post-content">After my mom got cancer, my family's theme song became "I Hope You Dance", by Lee Ann Womack. It's a reminder to
-                    get up and take advantage of life, rather than watching it pass by.</p><br/>
-                  </ol>
+
+                  <p class="post-content">After her postdoctoral work, Margarite began working at St Jude Children's
+                  Research Hospital in Memphis, Tennessee to pursue her passion for helping people, by developing
+                  current research to cure patients. Here, she published her first academic article in the Science
+                  publication, “Molecular Therapy.” A few years later, she joined the clinical team at the Bruce Lefroy
+                  Centre for Genetic Health Research at the Murdoch Children's Research Institute in Australia,
+                  where she works to this day and has published 15 more scholarly works. She helped develop cell
+                  and gene therapies for Friedreich’s Ataxia, a neurodegenerative disease that affects children.
+                  The goal of her research is to understand disease mechanisms and develop safe, effective therapies
+                  for Friedreich ataxia that readily translate to the clinic. As a result, therapies services will be
+                  more accessible to patients from different backgrounds who are diagnosed with Friedreich’s Ataxia
+                  backgrounds.</p><br/>
+
+
+                  <p class="post-content">As Marguerite’s career has blossomed, she has devoted herself to
+                  inspiring more women and students into STEM. In 2017, she became the Executive Director for the
+                  Industry Mentoring Network for STEM. ” She serves on the steering committee of the Science in
+                  Australia Gender Equity Forum and is the co-founder of Women in Science, Australia. Reflecting on
+                  her work, she expresses that “I work with a lot of young people in research and sometimes you have
+                  to say to them “don’t worry, be happy!” Try not to worry about the future. Just embrace it and go for it."
+                  Having received numerous awards for her research and leadership, Marguerite says all of it was possible
+                  due to her “sheer and utter determination.” Marguerite’s success is living, breathing, proof that
+                  there is a place for women in STEM, in research, in the hard sciences. She admits that it is a
+                  difficult path, yet the obstacles look surmountable, and the rewards large because of women like
+                  Marguerite.</p><br/>
+
 
                 </div>
             </div>
               {/*for the blog post author*/}
               <div class = "author">
-                <div class = "author-left">
-                  <div> {/*note this div is necessary to formatting*/}
-                    <img
-                    src={process.env.PUBLIC_URL + "/potraits/victoria.jpg"}
-                    className="profile_img vivw_img"
-                    alt="victoria-ruming"
-                    resizeMode="contain"
-                    />
-                  </div>
-                </div>
-                <div class = "author-right">
-                  <div class="title author-name">Victoria Ruming</div>
-                  <div class = "position"> Education Team </div>
+              <div class = "author-left">
+                <div> {/*note this div is necessary to formatting*/}
+                  <img
+                  src={process.env.PUBLIC_URL + "/potraits/vivw.jpg"}
+                  className="profile_img vivw_img"
+                  alt="vivian-wong"
+                  resizeMode="contain"
+                  />
                 </div>
               </div>
+            <div class = "author-right">
+              <div class="title author-name">Vivian Wong</div>
+              <div class = "position"> Education Executive</div>
+            </div>
+            </div>
+
               {/*End of blog posts*/}
             </div>
       );
