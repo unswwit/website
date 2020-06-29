f181778bd8dcab8fdf27fe2bef0b84886e106623
Lily Yang <lilyyang@Lilys-MacBook-Pro-2.local>
2020-06-29 17:26:48 +1000

    update for first page

--- src/marketingArchive.js
+++ src/marketingArchive.js
@@ -2,10 +2,7 @@
 import React, { Component } from "react";
 import "./style.css";
 import "./marketingArchive.css";
-
-import WCrushTan from "./initiatives/WCrushTan";
-import WCrushMayer from "./initiatives/WCrushMayer";
-import TechTrivia from "./initiatives/TechTrivia";
+import { Link } from "react-router-dom";
 
 class MarketingArchive extends Component {
   render() {
@@ -20,45 +17,17 @@ class MarketingArchive extends Component {
             </div>
             {/*End of Header*/}
 
-            <div id="parent">
-            {/*Left intiatives side bar*/}
-              <div id="side">
-                <div class="sideBar">
-                  {/*List of 2020 intatives*/}
-                  <div id="initiatives">
-                    <button class="init year active" onclick="filterSelection('2020')">2020</button>
-                    <button class="init" onclick="filterSelection('crush')">WIT Crush Wednesday</button>
-                    <button class="init" onclick="filterSelection('init2')">Initiative 2</button>
-                    <button class="init" onclick="filterSelection('init3')">Initiative 3</button>
-                  </div>
-                </div>
-              </div>
-
-              <div id="grid">
-                {/*Image collage*/}
-                <div class="gallery">
-                  <div class="row">
-                      <div class="image crush"><WCrushTan/></div>
-                      <div class="image trivia"><TechTrivia/></div>
-                  </div>
-                  <div class="row">
-                      <div class="image trivia"><TechTrivia/></div>
-                      <div class="image crush"><WCrushMayer/></div>
-                  </div>
-                  <div class="row">
-                      <div class="image crush"><WCrushTan/></div>
-                      <div class="image trivia"><TechTrivia/></div>
-                  </div>
-                  <div class="row">
-                      <div class="image trivia"><TechTrivia/></div>
-                      <div class="image crush"><WCrushMayer/></div>
-                  </div>
-                </div>
-              </div>
+            {/*Start of text*/}
+            <div class="introParagraph">
+              <p class="intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur molestie eu tellus sed facilisis. Aliquam eget interdum ipsum. Vivamus rutrum sapien velit, eget finibus enim pellentesque at. Ut sed felis nisi. Maecenas.</p>
             </div>
 
-
-
+            {/*2020 section*/}
+            <Link to="/marketing-archive/2020" style={{ textDecoration: 'none' }}>
+            <div class="yearSection">
+              <p class="year">2020</p>
+            </div>
+            </Link>
         </div>
     );
   }
