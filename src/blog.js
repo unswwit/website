fced88e975846bf03bd7d99754d1513b66928a81
aaalana <41637549+aaalana@users.noreply.github.com>
2020-06-29 12:57:20 +1000

    initial restructuring

--- src/blog.js
+++ src/blog.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import "./style.css";
 import "./blog.css";
 import { Link } from "react-router-dom";
+import BlogPreview from "./blog-preview";
 
 class Blog extends Component {
   render() {
@@ -20,191 +21,49 @@ class Blog extends Component {
             {/*Start of blog posts*/}
 
             {/*blog post 8*/}
-            <Link to="/blog/8" style={{ textDecoration: 'none' }}>
-            <div class="blog-post" style={{marginTop: "10vw", marginBottom: "5vw"}}>
-
-                <table cellpadding="0" style={{ width: "950px", height: "350px"}}>
-                <tr>
-                    <tr>
-                        <td>
-                            <img src={process.env.PUBLIC_URL + "/blog-covers/08-marguerite-evans-galea.png"} alt="preview" style={{width:"550px", height: "350px"}}/>
-                        </td>
-
-                        <td class= "blog-preview">
-                            <div class="blog-details">
-                                <div class="heading">
-                                WIT Crush Wednesday - Dr Marguerite Evans-Galea
-                                </div>
-                                <div class="date">24/06/2020</div>
-                                <div class="subheading">Take a look at Dr Marguerite Evans-Galea's
-                                journey into STEM as well as advice for students who are interested in
-                                pursuing a career in STEM</div>
-                            </div>
-                            <tr>
-                                <td>
-                                    <div class="author-pic">
-                                    <img
-                                    src={process.env.PUBLIC_URL + "/potraits/vivw.jpg"}
-                                    className="profile_img vivw_img"
-                                    alt="vivian-wong"
-                                    resizeMode="contain"
-                                    />
-                                    </div>
-                                </td>
-                                <td style={{width: "200px"}}>
-                                    <div class="auth">Vivian Wong</div>
-
-                                </td>
-                            </tr>
-                        </td>
-                    </tr>
-                </tr>
-                </table>
-            </div>
-            </Link>
-
+            <BlogPreview 
+            topMargin="10vw"
+            blogNo="8"
+            imgUrl="/blog-covers/08-marguerite-evans-galea.png"
+            heading="WIT Crush Wednesday - Dr Marguerite Evans-Galea"
+            date="24/06/2020"
+            subheading="Take a look at Dr Marguerite Evans-Galea's journey into STEM as well as advice for students who are interested in
+            pursuing a career in STEM"
+            authors={{"vivian-wong":["/potraits/vivw.jpg", "Vivian Wong"]}}
+            />
+            
             {/*blog post 7*/}
-            <Link to="/blog/7" style={{ textDecoration: 'none' }}>
-            <div class="blog-post" style={{marginTop: "5vw", marginBottom: "5vw"}}>
-
-                <table cellpadding="0" style={{ width: "950px", height: "350px"}}>
-                <tr>
-                    <tr>
-                        <td>
-                            <img src={process.env.PUBLIC_URL + "/blog-covers/07-ace-your-hackathon.png"} alt="preview" style={{width:"550px", height: "350px"}}/>
-                        </td>
-
-                        <td class= "blog-preview">
-                            <div class="blog-details">
-                                <div class="heading">
-                                How To Ace Your Hackathon
-                                </div>
-                                <div class="date">21/06/2020</div>
-                                <div class="subheading">Let's read what past WIT Hackathon winner has to say regarding their best tip to ace for your hackathon.</div>
-                            </div>
-                            <tr>
-                                <td>
-                                    <div class="author-pic">
-                                    <img
-                                    src={process.env.PUBLIC_URL + "/potraits/vivw.jpg"}
-                                    className="profile_img vivw_img"
-                                    alt="vivian-wong"
-                                    resizeMode="contain"
-                                    />
-                                    </div>
-                                </td>
-                                <td style={{width: "200px"}}>
-                                    <div class="auth">Vivian Wong</div>
-
-                                </td>
-                            </tr>
-                        </td>
-                    </tr>
-                </tr>
-                </table>
-            </div>
-            </Link>
-
-
-
+            <BlogPreview 
+            topMargin="5vw"
+            blogNo ="7"
+            imgUrl="/blog-covers/07-ace-your-hackathon.png"
+            heading="How To Ace Your Hackathon"
+            date="21/06/2020"
+            subheading="Let's read what past WIT Hackathon winner has to say regarding their best tip to ace for your hackathon."
+            authors={{"vivian-wong":["/potraits/vivw.jpg", "Vivian Wong"]}}
+            />
+            
+            {/*blog post 6*/}
+            <BlogPreview 
+            topMargin="5vw"
+            blogNo = "6" 
+            imgUrl="/blog-covers/06-fontaine-foxworth.png" 
+            heading="Introducing: Fontaine Foxworth, Product Manager of Google"
+            date="17/06/2020"
+            subheading="Let’s read what Fontaine had to say about her journey into tech."
+            authors={{"elisa-sanjurjo":["/potraits/victoria.jpg","Victoria Ruming"]}}
+            />
+            
             {/*blog post 5*/}
-            <Link to="/blog/6" style={{ textDecoration: 'none' }}>
-            <div class="blog-post" style={{marginTop: "5vw", marginBottom: "5vw"}}>
-
-                <table cellpadding="0" style={{ width: "950px", height: "350px"}}>
-                <tr>
-                    <tr>
-                        <td>
-                            <img src={process.env.PUBLIC_URL + "/blog-covers/06-fontaine-foxworth.png"} alt="preview" style={{width:"550px", height: "350px"}}/>
-                        </td>
-
-                        <td class= "blog-preview">
-                            <div class="blog-details">
-                                <div class="heading">
-                                Introducing: Fontaine Foxworth, Product Manager of Google
-                                </div>
-                                <div class="date">17/06/2020</div>
-                                <div class="subheading">Let’s read what Fontaine had to say about her journey into tech.</div>
-                            </div>
-                            <tr>
-                                <td>
-                                    <div class="author-pic">
-                                    <img
-                                    src={process.env.PUBLIC_URL + "/potraits/victoria.jpg"}
-                                    className="profile_img vivw_img"
-                                    alt="victoria-ruming"
-                                    resizeMode="contain"
-                                    />
-                                    </div>
-                                </td>
-                                <td style={{width: "200px"}}>
-                                    <div class="auth">Victoria Ruming</div>
-
-                                </td>
-                            </tr>
-                        </td>
-                    </tr>
-                </tr>
-                </table>
-            </div>
-            </Link>
-
-            {/*blog post 5*/}
-            <Link to="/blog/5" style={{ textDecoration: 'none' }}>
-            <div class="blog-post" style={{marginTop: "5vw", marginBottom: "5vw"}}>
-
-                <table cellpadding="0" style={{ width: "950px", height: "350px"}}>
-                <tr>
-                    <tr>
-                        <td>
-                            <img src={process.env.PUBLIC_URL + "/blog-covers/05-salina-jantarang.png"} alt="preview" style={{width:"550px", height: "350px"}}/>
-                        </td>
-
-                        <td class= "blog-preview">
-                            <div class="blog-details">
-                                <div class="heading">
-                                Interview - Salina Jantarang
-                                </div>
-                                <div class="date">12/06/2020</div>
-                                <div class="subheading">
-                                    Explore Salina's research journey as PHD student in chemical engineering as well as advices about finding success in university and beyond
-                                </div>
-                            </div>
-                            <tr>
-                                <td>
-                                    <div class="author-pic">
-                                    <img
-                                    src={process.env.PUBLIC_URL + "/potraits/vivw.jpg"}
-                                    className="profile_img vivw_img"
-                                    alt="vivian-wong"
-                                    resizeMode="contain"
-                                    />
-                                    </div>
-                                </td>
-                                <td style={{width: "200px"}}>
-                                    <div class="auth">Vivian Wong</div>
-
-                                </td>
-                                <td>
-                                    <div class="author-pic">
-                                    <img
-                                    src={process.env.PUBLIC_URL + "/potraits/victoria.jpg"}
-                                    className="profile_img vic_img"
-                                    alt="elisa-sanjurjo"
-                                    resizeMode="contain"
-                                    />
-                                    </div>
-                                </td>
-                                <td style={{width: "200px"}}>
-                                    <div class="auth">Victoria Ruming</div>
-                                </td>
-                            </tr>
-                        </td>
-                    </tr>
-                </tr>
-                </table>
-            </div>
-            </Link>
+            <BlogPreview 
+            topMargin="5vw"
+            blogNo = "5"
+            imgUrl="/blog-covers/05-salina-jantarang.png" 
+            heading="Interview - Salina Jantarang"
+            date="12/06/2020"
+            subheading="Explore Salina's research journey as PHD student in chemical engineering as well as advices about finding success in university and beyond"
+            authors={{"vivian-wong":["/potraits/vivw.jpg", "Vivian Wong"],"elisa-sanjurjo":["/potraits/victoria.jpg","Victoria Ruming"]}}
+            />
 
             {/*blog post 4*/}
             <Link to="/blog/4" style={{ textDecoration: 'none' }}>
