f96e0b65c8b896affad0d09c11d79e6e13823073
Lily Yang <lilyyang@Lilys-MacBook-Pro-2.local>
2020-06-29 17:27:33 +1000

    folder for different years in marketing archive

---
+++ src/marketing-years/2020.js
@@ -0,0 +1,67 @@
+//All necessary imports for this javascript
+import React, { Component } from "react";
+import ".././marketingArchive.css";
+import "./year.css";
+import ".././style.css";
+
+import WCrushTan from ".././initiatives/WCrushTan";
+import WCrushMayer from ".././initiatives/WCrushMayer";
+import TechTrivia from ".././initiatives/TechTrivia";
+
+class twentyTwenty extends Component {
+  render() {
+    return (
+        <div>
+
+            {/*Start of Header*/}
+            <div class="marketingCoverPhoto">
+                <div class="title">
+                    <h1>Marketing Archive</h1>
+                </div>
+            </div>
+            {/*End of Header*/}
+
+            <div id="parent">
+            {/*Left intiatives side bar*/}
+              <div id="side">
+                <div class="sideBar">
+                  {/*List of 2020 intatives*/}
+                  <div id="initiatives">
+                    <button class="init year active" onclick="filterSelection('2020')">2020</button>
+                    <button class="init" onclick="filterSelection('crush')">WIT Crush Wednesday</button>
+                    <button class="init" onclick="filterSelection('init2')">Initiative 2</button>
+                    <button class="init" onclick="filterSelection('init3')">Initiative 3</button>
+                  </div>
+                </div>
+              </div>
+
+              <div id="grid">
+                {/*Image collage*/}
+                <div class="gallery">
+                  <div class="row">
+                      <div class="image crush"><WCrushTan/></div>
+                      <div class="image trivia"><TechTrivia/></div>
+                  </div>
+                  <div class="row">
+                      <div class="image trivia"><TechTrivia/></div>
+                      <div class="image crush"><WCrushMayer/></div>
+                  </div>
+                  <div class="row">
+                      <div class="image crush"><WCrushTan/></div>
+                      <div class="image trivia"><TechTrivia/></div>
+                  </div>
+                  <div class="row">
+                      <div class="image trivia"><TechTrivia/></div>
+                      <div class="image crush"><WCrushMayer/></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+
+        </div>
+    );
+  }
+}
+export default twentyTwenty;
