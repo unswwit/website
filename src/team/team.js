7e471bc7a18c0a5f80188aa50ffc9becacad9f07
Lily Yang <lilyyang@Lilys-MacBook-Pro-2.local>
2020-07-18 19:03:11 +1000

    restructured subcom

--- src/team/team.js
+++ src/team/team.js
@@ -12,6 +12,7 @@ import Kelly from "./Kelly";
 import Rhea from "./Rhea";
 import VivianS from "./VivianS";
 import VivianW from "./VivianW";
+import SubCom from "./subcom";
 
 class OurTeam extends Component {
   render() {
@@ -41,7 +42,7 @@ class OurTeam extends Component {
                             <VivianS/>
                         </div>
                     </div>
-                        
+
                     <div className="exec_col">
                         <div class="profile">
                             <Amanda/>
@@ -49,13 +50,13 @@ class OurTeam extends Component {
                     </div>
                 </div>
 
-                <div className="exec_row">    
+                <div className="exec_row">
                     <div className="exec_col">
                         <div class="profile">
                             <Kelly/>
                         </div>
                     </div>
-                
+
                     <div className="exec_col">
                         <div class="profile">
                             <Elizabeth/>
@@ -65,27 +66,27 @@ class OurTeam extends Component {
 
                 <div class="exec_row">
                     <div class="exec_col">
-                        <div className="profile"> 
+                        <div className="profile">
                             <VivianW/>
                         </div>
                     </div>
 
                     <div class="exec_col">
-                        <div className="profile"> 
+                        <div className="profile">
                             <Gabrielle/>
                         </div>
                     </div>
                 </div>
-            
+
                 <div class="exec_row">
                     <div class="exec_col">
-                        <div className="profile"> 
+                        <div className="profile">
                             <Rhea/>
                         </div>
                     </div>
 
                     <div class="exec_col">
-                        <div className="profile"> 
+                        <div className="profile">
                             <Alison/>
                         </div>
                     </div>
@@ -93,312 +94,152 @@ class OurTeam extends Component {
 
                 <div class="exec_row">
                     <div class="exec_col">
-                    <div className="profile"> 
+                    <div className="profile">
                         <Elicia/>
                     </div>
                     </div>
 
                     <div class="exec_col">
-                    <div className="profile"> 
+                    <div className="profile">
                         <Felicia/>
                     </div>
                     </div>
                 </div>
             </div>
-            
+
             {/* Subcommittee section */}
             <h2 class="team-heading" style={{"marginTop":"2vw","paddingBottom":"5px"}}>OUR 2020 SUBCOMMITTEE TEAM</h2>
             <div className="subcom_section">
                 <h3 class="subcom-type">Education Team</h3>
-                <div class="subcom_row">
-                    <div class="subcom_col">
-                        <div class="profile">
-                            <p className="subcom_profile_name">Elisa Sanjurjo</p>
-                        </div>
-                    </div>
-                        
-                    <div className="subcom_longer_col">
-                        <div class="profile">
-                            <p className="subcom_profile_degree">Commerce/Science, 4th year</p>
-                        </div>
-                    </div>
-                </div>
+                <SubCom
+                name="Elisa Sanjurjo"
+                degree="Commerce/Science"
+                year="4th"
+                />
 
-                <div class="subcom_row">
-                    <div class="subcom_col">
-                        <div class="profile">
-                            <p className="subcom_profile_name">Georgie Mansfield</p>
-                        </div>
-                    </div>
-                        
-                    <div className="subcom_longer_col">
-                        <div class="profile">
-                            <p className="subcom_profile_degree">Chemical Engineering/Materials Science, 2nd year</p>
-                        </div>
-                    </div>
-                </div>
-               
-                <div class="subcom_row">
-                    <div class="subcom_col">
-                        <div class="profile">
-                            <p className="subcom_profile_name">Victoria Ruming</p>
-                        </div>
-                    </div>
-                        
-                    <div className="subcom_longer_col">
-                        <div class="profile">
-                            <p className="subcom_profile_degree">Mechatronic Engineering/Biomedical Engineering, 2nd year</p>
-                        </div>
-                    </div>
-                </div>
-               
+                <SubCom
+                name="Georgie Mansfield"
+                degree="Chemical Engineering/Materials Science"
+                year="2nd"
+                />
+
+                <SubCom
+                name="Victoria Ruming"
+                degree="Mechatronic Engineering/Biomedical Engineering"
+                year="2nd"
+                />
+
                 <h3 class="subcom-type">Events Team</h3>
-                <div class="subcom_row">
-                    <div class="subcom_col">
-                        <div class="profile">
-                            <p className="subcom_profile_name">Ariane Notaras</p>
-                        </div>
-                    </div>
-                        
-                    <div className="subcom_longer_col">
-                        <div class="profile">
-                            <p className="subcom_profile_degree">Commerce(Finance)/Information Systems, 2nd year</p>
-                        </div>
-                    </div>
-                </div>
+                <SubCom
+                name="Ariane Notaras"
+                degree="Commerce(Finance)/Information Systems"
+                year="2nd"
+                />
 
-                <div class="subcom_row">
-                    <div class="subcom_col">
-                        <div class="profile">
-                            <p className="subcom_profile_name">Jane Wang</p>
-                        </div>
-                    </div>
-                        
-                    <div className="subcom_longer_col">
-                        <div class="profile">
-                            <p className="subcom_profile_degree">Commerce/Information Systems, 2nd year</p>
-                        </div>
-                    </div>
-                </div>
+                <SubCom
+                name="Jane Wang"
+                degree="Commerce/Information Systems"
+                year="2nd"
+                />
 
-                <div class="subcom_row">
-                    <div class="subcom_col">
-                        <div class="profile">
-                            <p className="subcom_profile_name">Justin Clarke</p>
-                        </div>
-                    </div>
-                        
-                    <div className="subcom_longer_col">
-                        <div class="profile">
-                            <p className="subcom_profile_degree">Data Science and Decisions, 2nd year</p>
-                        </div>
-                    </div>
-                </div>
+                <SubCom
+                name="Justin Clarke"
+                degree="Data Science and Decisions"
+                year="2nd"
+                />
 
-                <div class="subcom_row">
-                    <div class="subcom_col">
-                        <div class="profile">
-                            <p className="subcom_profile_name">Nishmi Kapoor</p>
-                        </div>
-                    </div>
-                        
-                    <div className="subcom_longer_col">
-                        <div class="profile">
-                            <p className="subcom_profile_degree">Commerce/Advanced Science, 2nd year</p>
-                        </div>
-                    </div>
-                </div>
+                <SubCom
+                name="Nishmi Kapoor"
+                degree="Commerce/Advanced Sciences"
+                year="2nd"
+                />
 
-                <div class="subcom_row">
-                    <div class="subcom_col">
-                        <div class="profile">
-                            <p className="subcom_profile_name">Shirlee Salaria</p>
-                        </div>
-                    </div>
-                        
-                    <div className="subcom_longer_col">
-                        <div class="profile">
-                            <p className="subcom_profile_degree">Commerce/Information Systems, 2nd year</p>
-                        </div>
-                    </div>
-                </div>
+                <SubCom
+                name="Shirlee Salaria"
+                degree="Commerce/Information Systems"
+                year="2nd"
+                />
 
                 <h3 class="subcom-type">Externals Team</h3>
-                <div class="subcom_row">
-                    <div class="subcom_col">
-                        <div class="profile">
-                            <p className="subcom_profile_name">Angeni Bai</p>
-                        </div>
-                    </div>
-                        
-                    <div className="subcom_longer_col">
-                        <div class="profile">
-                            <p className="subcom_profile_degree">Computer Science, 1st year</p>
-                        </div>
-                    </div>
-                </div>
+                <SubCom
+                name="Angeni Bai"
+                degree="Computer Science"
+                year="1st"
+                />
 
-                <div class="subcom_row">
-                    <div class="subcom_col">
-                        <div class="profile">
-                            <p className="subcom_profile_name">Rue Teh</p>
-                        </div>
-                    </div>
-                        
-                    <div className="subcom_longer_col">
-                        <div class="profile">
-                            <p className="subcom_profile_degree">Software Engineering, 2nd year</p>
-                        </div>
-                    </div>
-                </div>
+                <SubCom
+                name="Rue Teh"
+                degree="Software Engineering"
+                year="2nd"
+                />
 
-                <div class="subcom_row">
-                    <div class="subcom_col">
-                        <div class="profile">
-                            <p className="subcom_profile_name">Srija Mukherjee</p>
-                        </div>
-                    </div>
-                        
-                    <div className="subcom_longer_col">
-                        <div class="profile">
-                            <p className="subcom_profile_degree">Software Engineering, 1st year</p>
-                        </div>
-                    </div>
-                </div>
+                <SubCom
+                name="Srija Mukherjee"
+                degree="Software Engineering"
+                year="1st"
+                />
 
                 <h3 class="subcom-type">Human Resources Team</h3>
-                <div class="subcom_row">
-                    <div class="subcom_col">
-                        <div class="profile">
-                            <p className="subcom_profile_name">Claire Yu</p>
-                        </div>
-                    </div>
-                        
-                    <div className="subcom_longer_col">
-                        <div class="profile">
-                            <p className="subcom_profile_degree">Commerce/Information System, 1st year</p>
-                        </div>
-                    </div>
-                </div>
+                <SubCom
+                name="Claire Yu"
+                degree="Commerce/Information System"
+                year="1st"
+                />
 
-                <div class="subcom_row">
-                    <div class="subcom_col">
-                        <div class="profile">
-                            <p className="subcom_profile_name">Victoria Yu</p>
-                        </div>
-                    </div>
-                        
-                    <div className="subcom_longer_col">
-                        <div class="profile">
-                            <p className="subcom_profile_degree">Information Systems, 2nd year</p>
-                        </div>
-                    </div>
-                </div>
+                <SubCom
+                name="Victoria Yu"
+                degree="Information Systems"
+                year="2nd"
+                />
 
                 <h3 class="subcom-type">Information Technology Team</h3>
-                
-                <div class="subcom_row">
-                    <div class="subcom_col">
-                        <div class="profile">
-                            <p className="subcom_profile_name">Alana Hua</p>
-                        </div>
-                    </div>
-                        
-                    <div className="subcom_longer_col">
-                        <div class="profile">
-                            <p className="subcom_profile_degree">Software Engineering/Biomedical Engineering, 3rd year</p>
-                        </div>
-                    </div>
-                </div>
+                <SubCom
+                name="Alana Hua"
+                degree="Software Engineering/Biomedical Engineering"
+                year="3rd"
+                />
 
-                <div class="subcom_row">
-                    <div class="subcom_col">
-                        <div class="profile">
-                            <p className="subcom_profile_name">Chelsea Chaffey</p>
-                        </div>
-                    </div>
-                        
-                    <div className="subcom_longer_col">
-                        <div class="profile">
-                            <p className="subcom_profile_degree">Computer Science, 1st year</p>
-                        </div>
-                    </div>
-                </div>
+                <SubCom
+                name="Chelsea Chaffey"
+                degree="Computer Science"
+                year="1st"
+                />
 
-                <div class="subcom_row">
-                    <div class="subcom_col">
-                        <div class="profile">
-                            <p className="subcom_profile_name">Gianfelix Goenawan</p>
-                        </div>
-                    </div>
-                        
-                    <div className="subcom_longer_col">
-                        <div class="profile">
-                            <p className="subcom_profile_degree">Computer Science/Commerce, 2nd year</p>
-                        </div>
-                    </div>
-                </div>
+                <SubCom
+                name="Gianfelix Goenawan"
+                degree="Computer Science/Commerce"
+                year="2nd"
+                />
 
-                <div class="subcom_row">
-                    <div class="subcom_col">
-                        <div class="profile">
-                            <p className="subcom_profile_name">Lily Yang</p>
-                        </div>
-                    </div>
-                        
-                    <div className="subcom_longer_col">
-                        <div class="profile">
-                            <p className="subcom_profile_degree">Computer Science/Environmental Engineering, 1st year</p>
-                        </div>
-                    </div>
-                </div>
+                <SubCom
+                name="Lily Yang"
+                degree="Computer Science/Environmental Engineering"
+                year="1st"
+                />
 
                 <h3 class="subcom-type">Marketing Team</h3>
-                <div class="subcom_row">
-                    <div class="subcom_col">
-                        <div class="profile">
-                            <p className="subcom_profile_name">Anisa Islam</p>
-                        </div>
-                    </div>
-                        
-                    <div className="subcom_longer_col">
-                        <div class="profile">
-                            <p className="subcom_profile_degree">Media in Communications and Journalism, 1st year</p>
-                        </div>
-                    </div>
-                </div>
-               
-                <div class="subcom_row">
-                    <div class="subcom_col">
-                        <div class="profile">
-                            <p className="subcom_profile_name">Connie Shi</p>
-                        </div>
-                    </div>
-                        
-                    <div className="subcom_longer_col">
-                        <div class="profile">
-                            <p className="subcom_profile_degree">Commerce/Information Systems, 3rd year</p>
-                        </div>
-                    </div>
-                </div>
+                <SubCom
+                name="Anisa Islam"
+                degree="Media in Communications and Journalism"
+                year="1st"
+                />
 
-                <div class="subcom_row" style={{"marginBottom":"8vw"}}>
-                    <div class="subcom_col">
-                        <div class="profile">
-                            <p className="subcom_profile_name">Vy Kim Nguyen</p>
-                        </div>
-                    </div>
-                        
-                    <div className="subcom_longer_col">
-                        <div class="profile">
-                            <p className="subcom_profile_degree">Bioinformatics/Biomedical Engineering, 2nd year</p>
-                        </div>
-                    </div>
-                </div>
+                <SubCom
+                name="Connie Shi"
+                degree="Commerce/Information Systems"
+                year="3rd"
+                />
+
+                <SubCom
+                name="Vy Kim Nguyen"
+                degree="Bioinformatics/Biomedical Engineering"
+                year="2nd"
+                />
+
             </div>
 
             <footer>
-                <div className="footer_area"></div>
+                <div className="footer_area" style={{"marginTop":"8vw"}}></div>
             </footer>
         </div>
     );
