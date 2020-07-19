45ac00ef26e63a6d50213eddc3e4840ecfe40362
aaalana <41637549+aaalana@users.noreply.github.com>
2020-07-20 09:57:16 +1000

    fixing navbar links

--- src/menu.js
+++ src/menu.js
@@ -30,28 +30,28 @@ class Menu extends Component{
               this.state.open ?
                 <div class="menu-list">
                     <ul class="navbar-nav menu-bar">
-                        <div class="menu-box"><li class="menu-item"><NavLink to="/">HOME</NavLink></li></div>
-                        <div class="menu-box"><li class="menu-item"><NavLink to="/events">EVENTS</NavLink></li></div>
-                        <div class="menu-box"><li class="menu-item"><NavLink to="/our-team">TEAM</NavLink></li></div>
-                        <div class="menu-box"><li class="menu-item"><NavLink to="/sponsors">SPONSORS</NavLink></li></div>
-                        <div class="menu-box"><li class="menu-item" onClick={this.handleClick}>
-                            <div class="dropdown">
+                        <NavLink class="menu-link" to="/"style={{textDecoration: "none"}}><div class="menu-box"><li class="menu-item">HOME</li></div></NavLink>
+                        <NavLink class="menu-link" to="/events" style={{textDecoration: "none"}}><div class="menu-box"><li class="menu-item">EVENTS</li></div></NavLink>
+                        <NavLink class="menu-link" to="/our-team" style={{textDecoration: "none"}}><div class="menu-box"><li class="menu-item">TEAM</li></div></NavLink>
+                        <NavLink class="menu-link"to="/sponsors" style={{textDecoration: "none"}}><div class="menu-box"><li class="menu-item">SPONSORS</li></div></NavLink>
+                        <div><li class="menu-item">
+                            <div class="menu-box dropdown" onClick={this.handleClick}>
                                 <div class="dropdown-toggle">
-                                    <span class="menuTitle">RESOURCES</span>
+                                    <span class="menu-item menuTitle">RESOURCES</span>
                                 </div>
                             </div>
                             {
                             this.state.showDD ?
                             <div>
-                                <div class="menu-box"><li class="menu-item"><NavLink to="/blog">BLOG</NavLink></li></div>
-                                <div class="menu-box"> <li class="menu-item"><NavLink to="/publications">PUBLICATIONS</NavLink></li></div>
-                                <div class="menu-box"> <li class="menu-item"><NavLink to="/blog">MARKETING ARCHIVES</NavLink></li></div>
+                                <NavLink class="menu-link" to="/blog" style={{textDecoration: "none"}}><div class="menu-box"><li class="menu-item">BLOG</li></div></NavLink>
+                                <NavLink class="menu-link" to="/publications" style={{textDecoration: "none"}}><div class="menu-box"><li class="menu-item">PUBLICATIONS</li></div></NavLink>
+                                <NavLink class="menu-link" to="/marketing-archive" style={{textDecoration: "none"}}><div class="menu-box"><li class="menu-item">MARKETING ARCHIVES</li></div></NavLink>
                             </div>:null
                             }
                         </li></div>
                     
-                        <div class="menu-box"><li class="menu-item"><NavLink to="/join-us">JOIN</NavLink></li></div>
-                        <div class="menu-box"><li class="menu-item" style={{marginRight:"15px"}}><NavLink to="/contact-us">CONTACT</NavLink></li></div>
+                        <NavLink class="menu-link" to="/join-us" style={{textDecoration: "none"}}><div class="menu-box"><li class="menu-item">JOIN</li></div></NavLink>
+                        <NavLink class="menu-link" to="/contact-us" style={{textDecoration: "none"}}><div class="menu-box"><li class="menu-item" style={{marginRight:"15px"}}>CONTACT</li></div></NavLink>
                     </ul>
                 </div>:null
             }
