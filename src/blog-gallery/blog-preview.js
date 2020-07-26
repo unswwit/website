f3854534c8ff58f6d1be394576de66a6e65a3fdc
aaalana <41637549+aaalana@users.noreply.github.com>
2020-07-26 20:00:02 +1000

    removing table format

--- src/blog-gallery/blog-preview.js
+++ src/blog-gallery/blog-preview.js
@@ -12,46 +12,40 @@ class BlogPreview extends Component {
                 <Link to={ "/blog/" + this.props.blogNo } style={{ textDecoration: 'none' }}>
                 <div class="blog-post" style={{marginTop:this.props.topMargin, marginBottom:this.props.bottomMargin}}>
 
-                    <table class="blog-table" cellpadding="0">
-                    <tr>
-                        <tr>
-                            <td>
-                                <img class="preview-pic" src={process.env.PUBLIC_URL + this.props.imgUrl} alt="preview" style={{width:"35.807vw", height: "22.786vw"}}/>
-                            </td>
-                            <td class= "blog-preview">
-                                <div class="blog-details">
-                                    <div class="heading">
-                                    {this.props.heading}
-                                    </div>
-                                    <div class="date">{this.props.date}</div>
-                                    <div class="subheading">{this.props.subheading}</div>
+                    <div class="blog-table">
+                        <div class="left-col"> 
+                            <img class="preview-pic" src={process.env.PUBLIC_URL + this.props.imgUrl} alt="preview" style={{width:"35.807vw", height: "22.786vw"}}/>
+                        </div>
+                        <div class= "blog-preview right-col">
+                            <div class="blog-details">
+                                <div class="heading">
+                                {this.props.heading}
                                 </div>
-                                <tr>
-                                {
-                                Object.keys(this.props.authors).map((key, index) => ( 
-                                    <td key={key}>
-                                        <td>
-                                        <div class="author-pic">
-                                        <img
-                                        src={process.env.PUBLIC_URL + this.props.authors[key][0]}
-                                        className="profile_img vivw_img"
-                                        alt={key}
-                                        resizeMode="contain"
-                                        />
-                                        </div>
-                                        </td>
-                                    
-                                        <td style={{width: "200px"}}>
-                                            <div class="auth">{this.props.authors[key][1]}</div>
-                                        </td>
+                                <div class="date">{this.props.date}</div>
+                                <div class="subheading">{this.props.subheading}</div>
+                            </div>
+                            {
+                            Object.keys(this.props.authors).map((key, index) => ( 
+                                <td key={key}>
+                                    <td>
+                                    <div class="author-pic">
+                                    <img
+                                    src={process.env.PUBLIC_URL + this.props.authors[key][0]}
+                                    className="profile_img vivw_img"
+                                    alt={key}
+                                    resizeMode="contain"
+                                    />
+                                    </div>
                                     </td>
-                                ))
-                                }
-                                </tr>
-                            </td>
-                        </tr>
-                    </tr>
-                    </table>
+                                
+                                    <td style={{width: "200px"}}>
+                                        <div class="auth">{this.props.authors[key][1]}</div>
+                                    </td>
+                                </td>
+                            ))
+                            }
+                        </div>
+                    </div>
                 </div>
                 </Link>
                 {/*End of blog posts*/}
