5806e14fb449795c3bc318b3c262401371d09031
Elicia Au Duong <eliciaauduong@outlook.com>
2020-07-27 12:02:52 +1000

    first publication release

---
+++ build/precache-manifest.7f5e61ac2787951bc12a8c17a93e418c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283f1427d273514719539fcc9abdee6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c05279b28de290ec7add",
+    "url": "/static/css/2.ace36aee.chunk.css"
+  },
+  {
+    "revision": "7dd4e8b25025e321ff41",
+    "url": "/static/css/main.248026d3.chunk.css"
+  },
+  {
+    "revision": "c05279b28de290ec7add",
+    "url": "/static/js/2.96634c42.chunk.js"
+  },
+  {
+    "revision": "748d5fd8a7e7493724665d4098123404",
+    "url": "/static/js/2.96634c42.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7dd4e8b25025e321ff41",
+    "url": "/static/js/main.a469ca4b.chunk.js"
+  },
+  {
+    "revision": "3bcb6824372899d729c8",
+    "url": "/static/js/runtime-main.f32eaec2.js"
+  }
+]);
