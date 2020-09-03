7517780355a5cc0c534b73576fd3814ffc1e0106
aaalana <41637549+aaalana@users.noreply.github.com>
2020-09-03 16:13:00 +1000

    mobile layout draft

--- src/join/joinUs.js
+++ src/join/joinUs.js
@@ -15,10 +15,10 @@ class JoinUs extends React.Component {
                 <div className="joinUsHeader">
                     {/* Cover Photo */}
                     {/* Main Title, and Subtitle Area */}
-                        <div className="title_area">
-                            <h1 className="centre">Join Us</h1>
-                        </div>
+                    <div className="title_area">
+                        <h1 className="centre">Join Us</h1>
                     </div>
+                </div>
                 <div className = 'joinUsBody'>
                     <img src={process.env.PUBLIC_URL + '/join-us-map.png'} 
                         width = "100%" 
@@ -26,50 +26,49 @@ class JoinUs extends React.Component {
                         alt="map"
                     />
                     <div className = 'joinUsRight'>
-                    <h2>Connect with us</h2>
-                    <div className = 'joinButtons'>
-                        <a href={fbPageUrl}>
-                            <img src={process.env.PUBLIC_URL + '/facebook.png'} 
-                                title = 'Facebook Page' 
-                                alt="Facebook Page" 
-                                resizeMode='contain'
-                                style={{width:'100%', height:'100%'}}
-                            />
-                        </a>
-                        <a href={fbGroupUrl}>
-                            <img src={process.env.PUBLIC_URL + '/facebook-group.png'} 
-                                title = 'Facebook Group' 
-                                alt="Facebook Group" 
-                                resizeMode='contain'
-                                style={{width:'100%', height:'100%'}} 
-                            />
-                        </a>
-                        <a href={linkedInUrl}>
-                            <img src={process.env.PUBLIC_URL + '/linkedin.png'} 
-                                title = 'Linked-In'
-                                alt="Linked-In" 
-                                resizeMode='contain'
-                                style={{width:'100%', height:'100%'}} 
-                            />
-                        </a>
-                        <a href={instagramUrl}>
-                            <img src={process.env.PUBLIC_URL + '/instagram.png'} 
-                                title = 'Instagram' 
-                                alt="Instagram" 
-                                resizeMode='contain'
-                                style={{width:'100%', height:'100%'}} 
-                            />
-                        </a>
-                        <a href={spARCUrl}>
-                            <img src={process.env.PUBLIC_URL + '/sparc.png'} 
-                                title = 'spARC'
-                                alt="spARC" 
-                                resizeMode='contain'
-                                style={{width:'100%', height:'100%'}} 
-                            />
-                        </a>
-                    </div>
-
+                        <h2>Connect with us</h2>
+                        <div className = 'joinButtons'>
+                            <a href={fbPageUrl}>
+                                <img src={process.env.PUBLIC_URL + '/facebook.png'} 
+                                    title = 'Facebook Page' 
+                                    alt="Facebook Page" 
+                                    resizeMode='contain'
+                                    style={{width:'100%', height:'100%'}}
+                                />
+                            </a>
+                            <a href={fbGroupUrl}>
+                                <img src={process.env.PUBLIC_URL + '/facebook-group.png'} 
+                                    title = 'Facebook Group' 
+                                    alt="Facebook Group" 
+                                    resizeMode='contain'
+                                    style={{width:'100%', height:'100%'}} 
+                                />
+                            </a>
+                            <a href={linkedInUrl}>
+                                <img src={process.env.PUBLIC_URL + '/linkedin.png'} 
+                                    title = 'Linked-In'
+                                    alt="Linked-In" 
+                                    resizeMode='contain'
+                                    style={{width:'100%', height:'100%'}} 
+                                />
+                            </a>
+                            <a href={instagramUrl}>
+                                <img src={process.env.PUBLIC_URL + '/instagram.png'} 
+                                    title = 'Instagram' 
+                                    alt="Instagram" 
+                                    resizeMode='contain'
+                                    style={{width:'100%', height:'100%'}} 
+                                />
+                            </a>
+                            <a href={spARCUrl}>
+                                <img src={process.env.PUBLIC_URL + '/sparc.png'} 
+                                    title = 'spARC'
+                                    alt="spARC" 
+                                    resizeMode='contain'
+                                    style={{width:'100%', height:'100%'}} 
+                                />
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
