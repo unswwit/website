3c4b651c6f3f48d5055f9c20b549339cebed2c87
aaalana <41637549+aaalana@users.noreply.github.com>
2020-09-28 13:31:57 +1000

    fixing header component to stay on the screen

--- src/header.js
+++ src/header.js
@@ -5,20 +5,19 @@ import "./blog-post/blog-post.css";
 class PageHeader extends Component {
     render() {
       return (
-
         <div class="coverPhoto"> {/*style={{backgroundImage: process.env.PUBLIC_URL + this.props.imgUrl}}*/}
           {/*dark overlay*/}
           <div class="dark"></div>
-          {/*header image*/}
 
+          {/*header image*/}
           <img src={process.env.PUBLIC_URL + this.props.imgUrl} alt="blog-header"
           className = "cover_image"/>
+
           {/*text*/}
           <div class="title">
               <h1>{this.props.title}</h1>
           </div>
         </div>
-
       );
     }
   }
