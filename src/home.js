7f56187ad7dd14e094268949c607d3334ea59315
chelffey <chaffeychelsea@gmail.com>
2020-10-11 11:37:27 +1100

    updated event page upcoming and home page event

--- src/home.js
+++ src/home.js
@@ -50,7 +50,7 @@ class Home extends Component{
 								<p>Sponsors</p>
 							</div>
 							<div>
-								<span><CountUp end={1976} duration={5}/></span>
+								<span><CountUp end={2063} duration={5}/></span>
 								<p>Facebook Followers</p>
 							</div>
 						</div>
@@ -76,8 +76,8 @@ class Home extends Component{
 						</a> */}
 					</div>
 					<div class="event-1">
-						<a href="https://www.facebook.com/events/913508185831961/">
-							<img src={process.env.PUBLIC_URL + '/event-covers/cloud-computing.jpg'} alt="banner"/>
+						<a href="https://www.facebook.com/events/2087909558012273">
+							<img src={process.env.PUBLIC_URL + './event-covers/31-oct-2020.jpg'} alt="Data Analytics Competition!"/>
 						</a>
 					</div>
 				</div>
