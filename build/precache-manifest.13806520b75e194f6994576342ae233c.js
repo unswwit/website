7d26860c806728f32471c02571468ab6f5a688d1
aaalana <41637549+aaalana@users.noreply.github.com>
2020-10-25 12:47:35 +1100

    redeploying blog 34

---
+++ build/precache-manifest.13806520b75e194f6994576342ae233c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73b931963937f44f01c24d36b16a6628",
+    "url": "/index.html"
+  },
+  {
+    "revision": "933c8afa2e2eba2ed661",
+    "url": "/static/css/2.aa13a6bf.chunk.css"
+  },
+  {
+    "revision": "d922e476d2d97b8cbfbb",
+    "url": "/static/css/main.f99f98f6.chunk.css"
+  },
+  {
+    "revision": "933c8afa2e2eba2ed661",
+    "url": "/static/js/2.ebe184be.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.ebe184be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d922e476d2d97b8cbfbb",
+    "url": "/static/js/main.6501e47a.chunk.js"
+  },
+  {
+    "revision": "3e60011f19d0b2029963",
+    "url": "/static/js/runtime-main.0625ef8c.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0f41e60ce368a4d6e45ea4652ed0ed33.js"
+  "/precache-manifest.13806520b75e194f6994576342ae233c.js"
 );
 
 self.addEventListener('message', (event) => {
