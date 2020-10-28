2dac7f92675c348e5fb770390fd674cc15b1585a
aaalana <41637549+aaalana@users.noreply.github.com>
2020-10-28 20:41:53 +1100

    puppeteer attempt

--- backend/routes/testAPI.js
+++ backend/routes/testAPI.js
@@ -6,3 +6,24 @@ router.get('/', function (req, res) {
 })
 
 module.exports = router;
+
+const puppeteer = require('puppeteer');
+
+async function run() {
+  try {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1920, height: 926 });
+    await page.goto(`https://developer.mozilla.org/en-US/docs/Web/API/Element/outerHTML`);
+    const data = await page.evaluate(() =>  document.querySelector('h2 #Syntax'));
+        //document.getElementsByClassName('oajrlxb2');
+        //document.querySelector('a').outerHTML
+
+    console.log(data);
+    await browser.close();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+run();
