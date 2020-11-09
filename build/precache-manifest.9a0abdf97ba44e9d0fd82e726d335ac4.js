a8c7a03774bed2f23d74f35ba5173ece0c964f2a
aaalana <41637549+aaalana@users.noreply.github.com>
2020-11-09 22:31:14 +1100

    capitalising author details

---
+++ build/precache-manifest.9a0abdf97ba44e9d0fd82e726d335ac4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a6dc8fe90f6b7976e736bffe9e71df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "933c8afa2e2eba2ed661",
+    "url": "/static/css/2.aa13a6bf.chunk.css"
+  },
+  {
+    "revision": "ef8942a16ce84be32e06",
+    "url": "/static/css/main.4223854f.chunk.css"
+  },
+  {
+    "revision": "933c8afa2e2eba2ed661",
+    "url": "/static/js/2.ebe184be.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.ebe184be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef8942a16ce84be32e06",
+    "url": "/static/js/main.4a7393fe.chunk.js"
+  },
+  {
+    "revision": "3e60011f19d0b2029963",
+    "url": "/static/js/runtime-main.0625ef8c.js"
+  }
+]);
