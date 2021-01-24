742a72f78c9ab3b2f2598ce666e3e18ef45d7403
aaalana <41637549+aaalana@users.noreply.github.com>
2021-01-24 23:39:50 +1100

    adding .env to gitignore

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var cors = require("cors");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var testAPIRouter = require("./routes/testAPI");
+//var testAPIRouter = require("./routes/testAPI");
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/testAPI', testAPIRouter);
+//app.use('/testAPI', testAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET event covers */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
