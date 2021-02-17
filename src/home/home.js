1a101d09cf92f3fd0600dce925a16e6a39679b94
aaalana <41637549+aaalana@users.noreply.github.com>
2021-02-17 19:23:37 +1100

    making loading screen

--- src/home/home.js
+++ src/home/home.js
@@ -7,147 +7,170 @@ import { Modal, Backdrop, Fade } from "@material-ui/core";
 
 const Home = () => {
   const [open, setOpen] = React.useState(false);
+  const [sourceLoading, setSourceLoading] = React.useState(true);
+  const [coverPhoto, setCoverPhoto] = React.useState(null);
 
-  //start webpage at the top
+  // start webpage at the top
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
+  // control when to stop loading
+  useEffect(() => {
+    const img = new Image();
+    img.src = "/headers/header-1.png";
+    img.onload = () => {     
+      setCoverPhoto(img.src);
+      setTimeout(() => {
+        setSourceLoading(false);
+      }, 2000);
+    }
+  }, [])  
+
+  // close the newsletter modal
   const callbackModal = () => {
     setOpen(false);
   };
-
+  
   return (
     <div>
-      {/* Start of Header */}
-      <div className={styles.contain}>
-        <div className={styles.headline}>
-          <h1>UNSW</h1>
-          <h1>Women In</h1>
-          <h1>Technology</h1>
-          <p> Empowering and inspiring the architects of change</p>
+      {sourceLoading ? (
+        <div className="loadingBackground">
+          Loading...
         </div>
-      </div>
-      {/* End of Header */}
-
-      {/* Start of Description */}
-      <div className={styles.description}>
-        <div className={styles.descriptionLeft}>
-          <h1>COLLABORATE. INSPIRE. CHANGE.</h1>
-        </div>
-        <div className={styles.descriptionRight}>
-          <p>
-            Formed in late 2016, we have grown to be a platform that empowers,
-            unites and up-skills female and male students alike that are
-            passionate about our mission. Women in Technology (WIT) aims to
-            cultivate future leaders that are prepared to challenge the
-            prejudices and bring change into the industry while building a
-            strong community where they can find lasting friendships and
-            support.
-          </p>
-        </div>
-      </div>
-      {/* End of Description */}
-
-      {/* Start of Statistics */}
-      <div className={styles.stats}>
-        <img
-          src={`${process.env.PUBLIC_URL}./stats-background-1.png`}
-          className={styles.statsBackground}
-          alt="banner"
-        />
-        <div>
-          <div className={styles.number}>
-            <div>
-              <CountUp end={30} duration={5} />  
-              <p>Team Members</p>
-            </div>
-            <div>
-              <CountUp end={15} duration={5} />
-              <p>Sponsors</p>
-            </div>
-            <div>
-              <CountUp end={2692} duration={4} />
-              <p>Facebook Followers</p>
+      )
+        :
+        (<>
+          {/* Start of Header */}
+          <div style={{backgroundImage: `url(${coverPhoto})`}} className={styles.contain}>
+            <div className={styles.headline}>
+              <h1>UNSW</h1>
+              <h1>Women In</h1>
+              <h1>Technology</h1>
+              <p> Empowering and inspiring the architects of change</p>
             </div>
           </div>
-        </div>
-      </div>
-      {/* End of Statistics */}
+          {/* End of Header */}
 
-      {/* Start of Upcoming Events */}
-      <div className={styles.events}>
-        <div className={styles.eventsDescription}>
-          <p id="about">
-            At WIT, our focus is on providing events that foster development
-            catering to first to final years. Whether it's personal, academic,
-            professional or social, we want our students to be the best
-            version of themselves. As part of our aim, we also connect current
-            and future students with alumni and industry sponsors to serve as
-            role models to inspire.
-          </p>
-          <p>
-            <Link to="/events" style={{ textDecoration: "none" }}>
-              See More Events
-            </Link>
-          </p>
-        </div>
-        <div className={styles.eventsTitle}>
-          <h1>EVENTS</h1>
-        </div>
-        <div className={styles.event1}>
-          <a 
-            href="https://www.facebook.com/events/254834122828459" 
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          {/* Start of Description */}
+          <div className={styles.description}>
+            <div className={styles.descriptionLeft}>
+              <h1>COLLABORATE. INSPIRE. CHANGE.</h1>
+            </div>
+            <div className={styles.descriptionRight}>
+              <p>
+              Formed in late 2016, we have grown to be a platform that empowers,
+              unites and up-skills female and male students alike that are
+              passionate about our mission. Women in Technology (WIT) aims to
+              cultivate future leaders that are prepared to challenge the
+              prejudices and bring change into the industry while building a
+              strong community where they can find lasting friendships and
+              support.
+              </p>
+            </div>
+          </div>
+          {/* End of Description */}
+
+          {/* Start of Statistics */}
+          <div className={styles.stats}>
             <img
-              src={`${process.env.PUBLIC_URL}/event-covers/2021/03-Chat-it-up-with-chatbot.gif`}
-              alt="Chat it up with chatbots"
+              src={`${process.env.PUBLIC_URL}./stats-background-1.png`}
+              className={styles.statsBackground}
+              alt="banner"
             />
-          </a>
-        </div>
-      </div>
-      {/* End of upcoming events area */}
-      
-      {/* Start of newsletter */}
-      <div className={styles.stats}>
-        <img
-          src={`${process.env.PUBLIC_URL}./stats-background-1.png`}
-          className={styles.statsBackground}
-          alt="banner"
-        />
-        <button className={styles.subscribeBtn} onClick={() => setOpen(true)}>
-          Subscribe to our newsletter
-        </button>
-        <Modal
-          aria-labelledby="spring-modal-title"
-          aria-describedby="spring-modal-description"
-          className={styles.signUpForm}
-          open={open}
-          onClose={() => setOpen(false)}
-          closeAfterTransition
-          BackdropComponent={Backdrop}
-          BackdropProps={{
-            timeout: 500,
-          }}
-        >
-          <>
-            <Fade in={open}> 
-              <NewsletterForm handleClose={callbackModal} />
-            </Fade> 
-          </>       
-        </Modal>
-      </div>
-      {/* End of Newsletter */}
+            <div>
+              <div className={styles.number}>
+                <div>
+                  <CountUp end={30} duration={5} />  
+                  <p>Team Members</p>
+                </div>
+                <div>
+                  <CountUp end={15} duration={5} />
+                  <p>Sponsors</p>
+                </div>
+                <div>
+                  <CountUp end={2692} duration={4} />
+                  <p>Facebook Followers</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* End of Statistics */}
 
-      <div className={styles.sponsors}>
-        <h1>OUR SPONSORS</h1>
-        <img
-          src={`${process.env.PUBLIC_URL}./sponsors-home.png`}
-          alt="banner"
-        />
-      </div>
+          {/* Start of Upcoming Events */}
+          <div className={styles.events}>
+            <div className={styles.eventsDescription}>
+              <p id="about">
+              At WIT, our focus is on providing events that foster development
+              catering to first to final years. Whether it's personal, academic,
+              professional or social, we want our students to be the best
+              version of themselves. As part of our aim, we also connect current
+              and future students with alumni and industry sponsors to serve as
+              role models to inspire.
+              </p>
+              <p>
+                <Link to="/events" style={{ textDecoration: "none" }}>
+                See More Events
+                </Link>
+              </p>
+            </div>
+            <div className={styles.eventsTitle}>
+              <h1>EVENTS</h1>
+            </div>
+            <div className={styles.event1}>
+              <a 
+                href="https://www.facebook.com/events/254834122828459" 
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={`${process.env.PUBLIC_URL}/event-covers/2021/03-Chat-it-up-with-chatbot.gif`}
+                  alt="Chat it up with chatbots"
+                />
+              </a>
+            </div>
+          </div>
+          {/* End of upcoming events area */}
+        
+          {/* Start of newsletter */}
+          <div className={styles.stats}>
+            <img
+              src={`${process.env.PUBLIC_URL}./stats-background-1.png`}
+              className={styles.statsBackground}
+              alt="banner"
+            />
+            <button className={styles.subscribeBtn} onClick={() => setOpen(true)}>
+            Subscribe to our newsletter
+            </button>
+            <Modal
+              aria-labelledby="spring-modal-title"
+              aria-describedby="spring-modal-description"
+              className={styles.signUpForm}
+              open={open}
+              onClose={() => setOpen(false)}
+              closeAfterTransition
+              BackdropComponent={Backdrop}
+              BackdropProps={{
+                timeout: 500,
+              }}
+            >
+              <>
+                <Fade in={open}> 
+                  <NewsletterForm handleClose={callbackModal} />
+                </Fade> 
+              </>       
+            </Modal>
+          </div>
+          {/* End of Newsletter */}
+
+          <div className={styles.sponsors}>
+            <h1>OUR SPONSORS</h1>
+            <img
+              src={`${process.env.PUBLIC_URL}./sponsors-home.png`}
+              alt="banner"
+            />
+          </div>
+        </>)}
     </div>
   );
 }
