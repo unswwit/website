fcb09d77ebb0951f4fadc2524ebf271808756e51
aaalana <41637549+aaalana@users.noreply.github.com>
2021-02-25 19:38:48 +1100

    add discord

--- src/join/joinUs.js
+++ src/join/joinUs.js
@@ -9,67 +9,47 @@ class JoinUs extends React.Component {
         const linkedInUrl = "https://www.linkedin.com/company/unsw-women-in-technology/";
         const instagramUrl = "https://www.instagram.com/wit.unsw/";
 
-        return (
-            <div>
-                <div className="joinUsHeader">
-                    {/* Cover Photo */}
-                    {/* Main Title, and Subtitle Area */}
-                    <div className="titleArea">
-                        <h1 className="centre">Join Us</h1>
-                    </div>
-                </div>
-                <div className = 'joinUsBody'>
-                    <div className = 'joinUsRight'>
-                        <h2>Connect with us</h2>
-                        <div className = 'joinButtons'>
-                            <a href={fbPageUrl}>
-                                <img src={process.env.PUBLIC_URL + '/facebook.png'} 
-                                    title = 'Facebook Page' 
-                                    alt="Facebook Page" 
-                                    resizeMode='contain'
-                                    style={{width:'100%', height:'100%'}}
-                                />
-                            </a>
-                            <a href={fbGroupUrl}>
-                                <img src={process.env.PUBLIC_URL + '/facebook-group.png'} 
-                                    title = 'Facebook Group' 
-                                    alt="Facebook Group" 
-                                    resizeMode='contain'
-                                    style={{width:'100%', height:'100%'}} 
-                                />
-                            </a>
-                            <a href={linkedInUrl}>
-                                <img src={process.env.PUBLIC_URL + '/linkedin.png'} 
-                                    title = 'Linked-In'
-                                    alt="Linked-In" 
-                                    resizeMode='contain'
-                                    style={{width:'100%', height:'100%'}} 
-                                />
-                            </a>
-                            <a href={instagramUrl}>
-                                <img src={process.env.PUBLIC_URL + '/instagram.png'} 
-                                    title = 'Instagram' 
-                                    alt="Instagram" 
-                                    resizeMode='contain'
-                                    style={{width:'100%', height:'100%'}} 
-                                />
-                            </a>
-                            <a className="lastBtn" href={spARCUrl}>
-                                <img src={process.env.PUBLIC_URL + '/sparc.png'} 
-                                    title = 'spARC'
-                                    alt="spARC" 
-                                    resizeMode='contain'
-                                    style={{width:'100%', height:'100%'}} 
-                                />
-                            </a>
-                        </div>
-                    </div>
-                    <img src={process.env.PUBLIC_URL + '/join-us-map.png'} 
-                        width = "100%" 
-                        className="googleMap" 
-                        alt="map"
-                    />
-                </div>
+    this.socials = {
+      spARC: ["https://member.arc.unsw.edu.au/s/clubdetail?clubid=0016F0000371W0xQAE", "sparc.png"],
+      "Facebook Page": ["https://www.facebook.com/unsw.wit/", "facebook.png"],
+      "Facebook Group": ["https://www.facebook.com/groups/unswwit/", "facebook-group.png"],
+      LinkedIn: ["https://www.linkedin.com/company/unsw-women-in-technology/", "linkedin.png"],
+      Instagram: ["https://www.instagram.com/wit.unsw/", "instagram.png"],
+      Youtube: ["https://www.youtube.com/channel/UCQ8PGe3P4ZuDSNCb9vCeTiw/videos/", "youtube.png"],
+      Twitch: ["https://www.twitch.tv/unswwit", "twitch.png"],
+      Spotify: ["https://open.spotify.com/show/1iWagdei1mVoyzg8TqbB2P", "spotify.png"],
+      Discord: ["https://discord.gg/BWY4tzM326", "discord.png"]
+    };
+  }
+
+  // hides the loading sign
+  hideSpinner() { 
+    this.setState({ loading: false });
+  }
+
+  render() {
+    return (
+      <div>
+        {/* Cover Photo */}
+        <PageHeader imgUrl="/headers/join-header-1.png" title="Join Us" />
+        <div className={styles.joinUsBody}>
+          <div className={styles.joinUsRight}>
+            <h2>Connect with us</h2>
+            <div className={styles.joinButtons}>
+              {Object.keys(this.socials).map((social, index) => {
+                return <a 
+                  className={styles.joinLink}
+                  key={index}
+                  href={this.socials[social][0]} 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={`${process.env.PUBLIC_URL}/icons/${this.socials[social][1]}`}
+                    alt={social}
+                  />
+                </a>
+              })}
             </div>
 
         );
