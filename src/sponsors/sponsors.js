10d9b43c46b0b14e8db997cdea24b915d9f963db
aaalana <41637549+aaalana@users.noreply.github.com>
2021-03-16 01:32:33 +1100

    updating stats, headers and sponsor links

--- src/sponsors/sponsors.js
+++ src/sponsors/sponsors.js
@@ -2,56 +2,42 @@ import React, { Component } from "react";
 import styles from "./sponsors.module.css";
 import PageHeader from ".././header";
 
-import Principal from "./Principal";
-import Major from "./Major";
-import SupportInkind from "./SupportInKind";
+const Sponsors = () => {
+  const majorSponsors = {
+    Accenture: [ "https://www.accenture.com/au-en", "accenture.png" ],
+    Atlassian: [ "https://www.accenture.com/au-en", "atlassian-major.png" ],
+    // Cba: [ "https://www.commbank.com.au/about-us/careers.html", "cba-major.png" ],
+    Ey: [ "https://www.ey.com/en_au", "EY.gif" ],
+    Fdm: [ "https://www.fdmgroup.com/en-au/au-home/", "fdm-group.png" ],
+    Google: [ "https://careers.google.com/", "google-major.png" ],
+    JaneStreet: [ "https://www.janestreet.com/", "jane-street.png" ],
+    McGrathNicol: [ "https://www.mcgrathnicol.com/", "mcgrathcicol-major.png" ],
+    Nine: [ "https://www.nineforbrands.com.au/", "nine-major.png" ],
+    Optiver: [ "https://www.optiver.com/", "optiver.png" ],
+    Pwc: [ "https://www.pwc.com.au/", "pwc-logo-png-transparent.png" ],
+    Quantium: [ "https://quantium.com/", "quantium-major.png" ],
+    Resmed: [ "https://www.resmed.com.au/", "ResMed.jpg" ],
+    Telstra: [ "https://www.telstra.com.au/", "telstra-large.png" ]
+  };
+  return (
+    <div>
+      {/* Cover Photo */}
+      <PageHeader imgUrl="/headers/sponsors-header-1.png" title="Sponsors" />
 
-class Sponsors extends Component {
-  render() {
-    return (
-      <div>
-        {/* Cover Photo */}
-        <PageHeader imgUrl="/headers/sponsors-header-1.png" title="Sponsors" />
+      <div id={styles.sponsorsBody}>
+        <p className={styles.subheader}>
+					Thank you to our sponsors for generously supporting our cause and collaborating with us to provide
+					our students invaluable opportunities.
+        </p>
 
-        <div id={styles.sponsorsBody}>
-          <p className={styles.subheader}>
-            Thank you to our sponsors for generously supporting our cause and
-            collaborating with us to provide our students invaluable opportunities.
-          </p>
-
-          {/* Principal Sponsors Area */}
-          <h2 className={styles.subsponsor}>Principal Sponsors</h2>
-          <div id={styles.majorContainer}>
-            <Principal
-              details={{
-                Telstra: [
-                  "https://www.telstra.com.au/",
-                  "/sponsors/telstra-large.png",
-                ],
-              }}
-            />
-          </div>
-
-          {/* Major Sponsors Area */}
-          <h2 className={styles.subsponsor}>Major Sponsors</h2>
-
-          <div id={styles.majorContainer}>
-            <Major
-              details={{
-                Amazon: [
-                  "https://www.amazon.jobs/en",
-                  "/sponsors/amazon-major.png",
-                ],
-              }}
-            />
-
-            <Major
-              details={{
-                Atlassian: [
-                  "https://www.atlassian.com/",
-                  "/sponsors/atlassian-major.png",
-                ],
-              }}
+        {/* Principal Sponsors Area */}
+        <h2 className={styles.subsponsor}>Principal Sponsors</h2>
+        <div id={styles.majorContainer}>
+          <a href="https://www.eucalyptus.vc/" target="_blank" rel="noopener noreferrer">
+            <img
+              className={styles.principal}
+              src={process.env.PUBLIC_URL + "/sponsors/2021/euc-logo.png"}
+              alt="Eucalyptus"
             />
 
             <Major
