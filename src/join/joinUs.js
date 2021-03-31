5fda8dbaa09053e55ca6210a9a9dccbbacc399d5
thefirewood <77281319+thefirewood@users.noreply.github.com>
2021-03-31 22:41:24 +0700

    add accordion

--- src/join/joinUs.js
+++ src/join/joinUs.js
@@ -2,6 +2,11 @@ import React from "react";
 import PageHeader from ".././header";
 import styles from "./joinUs.module.css";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 class JoinUs extends React.Component {
   constructor() {
@@ -123,8 +128,139 @@ class JoinUs extends React.Component {
             varies from year to year.
           </p>
         </div>
+        <h2 className={styles.header}>What to join</h2>
+        <div className={styles.descriptionHowto}>
+          <p>
+            WIT has several specialised portfolios that you can join as a subcommittee member. 
+            Each portfolio is led by our executives. Hover over one of the portfolios to find out 
+            more about the portfolio.
+          </p>
+        </div>
+        <h2 className={styles.header}>FAQ</h2>
+        <div className={styles.accordion}>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel4bh-content"
+            >
+              <Typography id={styles.accordionHeading}>
+                <div className={styles.accordionHeading}>
+                  <strong>
+                    Can non-female students apply for a subcommittee position?
+                  </strong>
+                </div>
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <p className={styles.accordionDetails}>
+                Yes, WIT encourages all students who are interested to apply for 
+                not just our subcommittee positions but also participate in any of our events. 
+              </p>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel4bh-content"
+            >
+              <Typography id={styles.accordionHeading}>
+                <div className={styles.accordionHeading}>
+                  <strong>
+                  Do I need a tech background to join? 
+                  </strong>
+                </div>
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <p className={styles.accordionDetails}> 
+                Not at all! We consider applicants from all degrees, 
+                as long as you have a passion for technology. 
+              </p>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel4bh-content"
+            >
+              <Typography id={styles.accordionHeading}>
+                <div className={styles.accordionHeading}>
+                  <strong>
+                    Do I need any prior experience in the portfolio that I applied?
+                  </strong>
+                </div>
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <p className={styles.accordionDetails}>
+                No. Although some technical experience for IT and Marketing 
+                may be beneficial, it is certainly not required.
+              </p>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel4bh-content"
+            >
+              <Typography id={styles.accordionHeading}>
+                <div className={styles.accordionHeading}>
+                  <strong>
+                    How can I make my application stand out?
+                  </strong>
+                </div>
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <p className={styles.accordionDetails}>
+                Be enthusiastic! Show us that you are excited to 
+                become a part of WIT and tell us how you can contribute unique value to the society.
+              </p>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel4bh-content"
+            >
+              <Typography id={styles.accordionHeading}>
+                <div className={styles.accordionHeading}>
+                  <strong>
+                    What is the time commitment like? 
+                  </strong>
+                </div>
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <p className={styles.accordionDetails}>
+                Like most societies, the time commitment expected 
+                for a subcommittee member will be approximately 2-4 hours per week.
+              </p>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel4bh-content"
+            >
+              <Typography id={styles.accordionHeading}>
+                <div className={styles.accordionHeading}>
+                  <strong>
+                    I’m not in 1st year, can I still join as a Subcommittee member?
+                  </strong>
+                </div>
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <p className={styles.accordionDetails}>
+                Yes, we will consider all applicants regardless of year and stage in degrees. 
+              </p>
+            </AccordionDetails>
+          </Accordion>
+        </div>
       </div>
     );
   }
 }
+
 export default JoinUs;
