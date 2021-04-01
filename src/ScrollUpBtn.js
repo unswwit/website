3e2ba56d15e528d48efe2540256b6ac24366ba34
Tiana Douroudis <tianadouroudis@Tianas-MacBook-Pro.local>
2021-04-01 19:17:18 +1100

    scroll up btn draft

---
+++ src/ScrollUpBtn.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import styles from "./modules.css"
+
+export default function ScrollUpBtn() {
+  const [isVisible, setIsVisible] = useState(false);
+  const [intervalId, setintervalId] = useState(0);
+
+  // Show button when page is scorlled upto given distance
+  const toggleVisibility = () => {
+    if (window.pageYOffset >= 20) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollStep = (props) => {
+    if (window.pageYOffset === 0) {
+        clearInterval(intervalId);
+    }
+    window.scroll(0, window.pageYOffset - props.scrollStepInPx);
+  }
+  
+  const scrollToTop = (props) => {
+    let intervalId = setInterval(scrollStep.bind(ScrollUpBtn), props.delayInMs);
+    setintervalId(intervalId);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+  }, []);
+
+  return (
+    <button title='Scroll to top' className={styles.scrollUp}
+     onClick={scrollToTop}>
+    <KeyboardArrowUpIcon color="#ffffff" ></KeyboardArrowUpIcon>
+    </button>
+  )
+}
