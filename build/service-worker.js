70a1ca5e5689597d24143d2d84c691ea6b61b998
aaalana <41637549+aaalana@users.noreply.github.com>
2021-04-06 09:09:19 +1000

    scroll to top button build release

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3815db72b90193ddc4331c6b02826a7b.js"
+  "/precache-manifest.3ac9eb91b3a145251f20fb99295bf61c.js"
 );
 
 self.addEventListener('message', (event) => {
