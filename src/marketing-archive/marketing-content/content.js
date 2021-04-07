bf4b13fa4c3bf46c6baff8bc9e867777d695e27b
aaalana <41637549+aaalana@users.noreply.github.com>
2021-04-07 21:41:00 +1000

    bringing back loading marketing archives

--- src/marketing-archive/marketing-content/content.js
+++ src/marketing-archive/marketing-content/content.js
@@ -1,6 +1,5 @@
 //All necessary imports for this javascript
 import React, { useEffect, useState } from "react";
-import ".././marketingArchive.module.css";
 import styles from "./content.module.css";
 import "../.././style.css";
 import Timeline from "../../Timeline";
@@ -13,13 +12,13 @@ import Tabletop from "tabletop";
 const MarketingContent = () => {
   const [content, setContent] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [year, setYear] = useState(2020);
+  const [year, setYear] = useState("2021");
   const [selectedCategory, setSelectedCategory] = useState("All");
   const categories = {
-    "All": "All", 
-    "Mascot": "mascot", 
-    "Memes": "memes", 
-    "Monday Motivation": "monday", 
+    All: "All",
+    Mascot: "mascot",
+    Memes: "memes",
+    "Monday Motivation": "monday",
     "WIT Crush Wednesday": "wcw",
     "Special Occasions": "special",
   };
@@ -29,95 +28,131 @@ const MarketingContent = () => {
     {
       value: 100,
       scaledValue: 2021,
-      label: "2021"
+      label: "2021",
     },
     {
       value: 0,
       scaledValue: 2020,
-      label: "2020"
+      label: "2020",
     },
   ];
 
   const valueToYear = {
     0: "2020",
-    100: "2021"
-  }
+    100: "2021",
+  };
 
   // set the year for the events timeline
   const handleYear = (newYear) => {
     setYear(newYear);
-  };  
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    Tabletop.init({
+      key: process.env.REACT_APP_GOOGLE_SHEETS,
+      callback: (googleData) => {
+        setLoading(false);
+        setContent(
+          googleData["marketing-archives"]["elements"]
+            .reverse()
+            .filter((item) => item.year === year)
+        );
+      },
+      simpleSheet: false,
+    });
+  }, [selectedCategory, year]);
 
   return (
     <>
       {/* Cover Photo */}
-      <PageHeader imgUrl="/headers/marketing-header.png" title="Marketing Archives" />
+      <PageHeader
+        imgUrl="/headers/marketing-header.png"
+        title="Marketing Archives"
+      />
       {/*End of Header*/}
 
       <div id={styles.parent}>
         {/*List of initiatives*/}
         <div className={styles.initiatives}>
-          {/* Start of categories */}      
+          {/* Start of categories */}
           <div className={styles.contentCategories}>
-            {Object.keys(categories).sort().map((category) => {
-              const chipColour = selectedCategory === categories[category] ? "#e85f5c": "#7F7F7F";
-              return <Chip 
-                key={category}
-                size="medium" 
-                label={category} 
-                style={{
-                  textTransform: "uppercase",
-                  backgroundColor: chipColour, 
-                  color: "white",
-                  margin: "5px",  
-                }}
-                onClick={() => {
-                  setLoading(true);
-                  setSelectedCategory(categories[category]);                
-                }}
-              />                
-            })}
-          </div>  
-          
+            {Object.keys(categories)
+              .sort()
+              .map((category) => {
+                const chipColour =
+                  selectedCategory === categories[category]
+                    ? "#e85f5c"
+                    : "#7F7F7F";
+                return (
+                  <Chip
+                    key={category}
+                    size="medium"
+                    label={category}
+                    style={{
+                      textTransform: "uppercase",
+                      backgroundColor: chipColour,
+                      color: "white",
+                      margin: "5px",
+                    }}
+                    onClick={() => {
+                      setLoading(true);
+                      setSelectedCategory(categories[category]);
+                    }}
+                  />
+                );
+              })}
+          </div>
+
           <div>
             {/* Timeline */}
-            <Timeline 
-              margin={"7%"}
-              page={"teams"}
+            <Timeline
+              margin={"2%"}
+              page={"marketing"}
               step={25}
-              valueToYear={valueToYear} 
-              marks={marks} 
-              updateYear={handleYear} 
+              valueToYear={valueToYear}
+              marks={marks}
+              updateYear={handleYear}
             />
           </div>
 
           <div id={styles.contentLoadingContainer}>
-            {loading && <CircularProgress
-              variant="indeterminate"
-              size={50}
-              thickness={5}
-              id={styles.contentLoading}
-            />}
+            {loading && (
+              <CircularProgress
+                variant="indeterminate"
+                size={50}
+                thickness={5}
+                id={styles.contentLoading}
+              />
+            )}
           </div>
 
           {/*Image collage*/}
-          {!loading && <ol className={styles.grid} id={styles.content}> 
-            {content
-              .filter((picture) => selectedCategory === "All" || picture.category.split(",").includes(selectedCategory))
-              .map((content, index) => {   
-                return <Initiative
-                  key={index}
-                  fb={content.link}
-                  imgUrl={`/initiatives/${year}/${content.img}`}
-                  alt={content.label}
-                  date={content.date} 
-                />
-              })}
-          </ol>}   
+          {!loading && (
+            <ol className={styles.grid} id={styles.content}>
+              {content
+                .filter(
+                  (picture) =>
+                    selectedCategory === "All" ||
+                    picture.category.split(",").includes(selectedCategory)
+                )
+                .map((content, index) => {
+                  return (
+                    <Initiative
+                      key={index}
+                      fb={content.link}
+                      imgUrl={`/initiatives/${year}/${content.img}`}
+                      alt={content.label}
+                      date={content.date}
+                    />
+                  );
+                })}
+            </ol>
+          )}
         </div>
         {/*End of Initiatives*/}
       </div>
     </>
   );
-}
+};
 export default MarketingContent;
