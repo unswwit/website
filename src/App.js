e721b760773f88f06f17297bcdbf8f5f5e08c3dd
thefirewood <77281319+thefirewood@users.noreply.github.com>
2021-05-08 15:58:54 +0700

    run prettier

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React, { Component } from "react";
-import {
-  HashRouter,
-  Route,
-  NavLink,
-  Switch,
-  Redirect,
-} from "react-router-dom";
+import { HashRouter, Route, NavLink, Switch, Redirect } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "semantic-ui-css/semantic.min.css";
 import "./style.css";
