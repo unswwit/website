7ae742192401c3687b710b6358094d0ad4cccbd0
Grace Kan <grace.kan@education.nsw.gov.au>
2021-05-23 17:22:16 +1000

    Opportunities
    added loading screen to opportunities

--- src/opportunities/opportunities.js
+++ src/opportunities/opportunities.js
@@ -1,73 +1,97 @@
 //All necessary imports for this javascript
 import React, { useEffect } from "react";
 import PageHeader from ".././header";
+import LoadingScreen from "../LoadingScreen";
 
 import ".././style.css";
 import styles from "./opportunities.module.css";
 //import OppCard from "./oppCard.js";
 
 const Opportunities = () => {
+  const [initialLoading, setinitialLoading] = React.useState(true);
+  const [sourceLoading, setSourceLoading] = React.useState(true);
+  const [coverPhoto, setCoverPhoto] = React.useState(null);
+
   useEffect(() => {
     window.scrollTo(0,0);
   }, []);
+
+  // control when to stop loading
+  useEffect(() => {
+    const img = new Image();
+    img.src = "/headers/header-1.jpg";
+    img.onload = () => {     
+      setCoverPhoto(img.src);
+      setTimeout(() => {
+        setSourceLoading(false);
+      }, 1000);
+    }
+  }, [])  
   
   return (
     <div>
-      {/* Cover Photo */}
-      <PageHeader
-        imgUrl="/headers/opportunities-header.jfif"
-        title="Opportunities"
-      />
+    {sourceLoading ? (
+      <LoadingScreen />
+    )
+      :
+      (
+      <div>
+        {/* Cover Photo */}
+        <PageHeader
+          imgUrl="/headers/opportunities-header.jfif"
+          title="Opportunities"
+        />
 
-      {/*start of active opportunies*/}
-      <h2 className={styles.oppSubheading}>Active Opportunities</h2>
-      <p className={styles.oppLookout} style={{ marginBottom: "100px" }}>
-        Keep a lookout here for upcoming opportunities!
+        {/*start of active opportunies*/}
+        <h2 className={styles.oppSubheading}>Active Opportunities</h2>
+        <p className={styles.oppLookout} style={{ marginBottom: "100px" }}>
+          Keep a lookout here for upcoming opportunities!
+          {/*
+          <OppCard
+              details={{
+                CommonwealthBank: [
+                  "/sponsors/2020/telstra-large.png",
+                  "7 July 2020",
+                  "Summer Vacationer",
+                  "Do you know that by being a Summer Vacationer at Telstra you could be considered for early offers and join the Graduate Program as soon as you graduate? Telstra will guide and support you on your journey, as you see what is possible in your career. Applications for Telstra’s Summer Vacation Program start July 7. Don’t miss out on this opportunity! #TeamTelstra #TelstraSummerVac",
+                  "https://www.youtube.com/watch?v=4R6Vy7QOgKA&t=4s  ",
+                ],
+              }}
+            />*/}
+        </p>
+        {/*Start of past opportunities*/}
         {/*
-        <OppCard
-            details={{
-              CommonwealthBank: [
-                "/sponsors/2020/telstra-large.png",
-                "7 July 2020",
-                "Summer Vacationer",
-                "Do you know that by being a Summer Vacationer at Telstra you could be considered for early offers and join the Graduate Program as soon as you graduate? Telstra will guide and support you on your journey, as you see what is possible in your career. Applications for Telstra’s Summer Vacation Program start July 7. Don’t miss out on this opportunity! #TeamTelstra #TelstraSummerVac",
-                "https://www.youtube.com/watch?v=4R6Vy7QOgKA&t=4s  ",
-              ],
-            }}
-          />*/}
-      </p>
-      {/*Start of past opportunities*/}
-      {/*
-      <h2 className={styles.oppSubheading}>Past Opportunities</h2>
-      <div className={styles.oppGrid}>
-        <div className={styles.container}>
-          <OppCard
-            details={{
-              Telstra: [
-                "/sponsors/2020/telstra-large.png",
-                "7 July 2020",
-                "Summer Vacationer",
-                "Do you know that by being a Summer Vacationer at Telstra you could be considered for early offers and join the Graduate Program as soon as you graduate? Telstra will guide and support you on your journey, as you see what is possible in your career. Applications for Telstra’s Summer Vacation Program start July 7. Don’t miss out on this opportunity! #TeamTelstra #TelstraSummerVac",
-                "https://www.youtube.com/watch?v=4R6Vy7QOgKA&t=4s  ",
-              ],
-            }}
-          />
+        <h2 className={styles.oppSubheading}>Past Opportunities</h2>
+        <div className={styles.oppGrid}>
+          <div className={styles.container}>
+            <OppCard
+              details={{
+                Telstra: [
+                  "/sponsors/2020/telstra-large.png",
+                  "7 July 2020",
+                  "Summer Vacationer",
+                  "Do you know that by being a Summer Vacationer at Telstra you could be considered for early offers and join the Graduate Program as soon as you graduate? Telstra will guide and support you on your journey, as you see what is possible in your career. Applications for Telstra’s Summer Vacation Program start July 7. Don’t miss out on this opportunity! #TeamTelstra #TelstraSummerVac",
+                  "https://www.youtube.com/watch?v=4R6Vy7QOgKA&t=4s  ",
+                ],
+              }}
+            />
 
-          <OppCard
-            details={{
-              EY: [
-                "/sponsors/2020/ey-major.png",
-                "7 July 2020",
-                "Breaking Down Barriers Program",
-                "When – Tuesday 21st July  Where – Virtual  Time – 10am – 12:30pm. Register at the link below!",
-                "https://globaleysurvey.ey.com/jfe/form/SV_43pqDbzTEfL4Lbf ",
-              ],
-            }}
-          />
-        </div>
-      </div>*/}
-      {/*End of opportunities grid table*/}
-    </div>
+            <OppCard
+              details={{
+                EY: [
+                  "/sponsors/2020/ey-major.png",
+                  "7 July 2020",
+                  "Breaking Down Barriers Program",
+                  "When – Tuesday 21st July  Where – Virtual  Time – 10am – 12:30pm. Register at the link below!",
+                  "https://globaleysurvey.ey.com/jfe/form/SV_43pqDbzTEfL4Lbf ",
+                ],
+              }}
+            />
+          </div>
+        </div>*/}
+        {/*End of opportunities grid table*/}
+      </div>
+      )}</div>
   );
-}
+};
 export default Opportunities;
