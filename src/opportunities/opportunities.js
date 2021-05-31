1b393cf51436435e633f7b0b2cece7d8d3f3d4ac
thefirewood <77281319+thefirewood@users.noreply.github.com>
2021-05-31 09:41:16 +0700

    run prettier

--- src/opportunities/opportunities.js
+++ src/opportunities/opportunities.js
@@ -77,22 +77,26 @@ const Opportunities = () => {
                     key={index}
                   >
                     <div>
-                      {(individualOpportunity.img.length ? (<img
-                        className={styles.oppImg}
-                        src={
-                          process.env.PUBLIC_URL +
-                          `/sponsors/2021/${individualOpportunity.img}`
-                        }
-                        alt={individualOpportunity.companyName}
-                      />) : (<img
-                        className={styles.oppImg}
-                        src={
-                          process.env.PUBLIC_URL +
-                          `/opportunities/${individualOpportunity.notSponsorImg}`
-                        }
-                        alt={individualOpportunity.companyName}
-                      />))}
-                      
+                      {individualOpportunity.img.length ? (
+                        <img
+                          className={styles.oppImg}
+                          src={
+                            process.env.PUBLIC_URL +
+                            `/sponsors/2021/${individualOpportunity.img}`
+                          }
+                          alt={individualOpportunity.companyName}
+                        />
+                      ) : (
+                        <img
+                          className={styles.oppImg}
+                          src={
+                            process.env.PUBLIC_URL +
+                            `/opportunities/${individualOpportunity.notSponsorImg}`
+                          }
+                          alt={individualOpportunity.companyName}
+                        />
+                      )}
+
                       {/* The image name could be found in the "public/sponsors/2021" folder */}
                       <div className={styles.oppDesc}>
                         <p className={styles.oppTypeAndLocation}>
@@ -106,11 +110,15 @@ const Opportunities = () => {
                         <p className={styles.oppTypeAndLocation}>
                           {individualOpportunity.location}
                         </p>
-                        {individualOpportunity.closeDate.length ? (                        <p className={styles.oppSummary}>
-                          Applications close: {individualOpportunity.closeDate}
-                          {/* The position should be in the format of the following example: 01/01/2021 */}
-                        </p>) : (<p className={styles.oppSummary}>
-                        </p>)}
+                        {individualOpportunity.closeDate.length ? (
+                          <p className={styles.oppSummary}>
+                            Applications close:{" "}
+                            {individualOpportunity.closeDate}
+                            {/* The position should be in the format of the following example: 01/01/2021 */}
+                          </p>
+                        ) : (
+                          <p className={styles.oppSummary}></p>
+                        )}
                         <p className={styles.oppSummary}>
                           {individualOpportunity.summary}
                         </p>
