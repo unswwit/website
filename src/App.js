d69ea848350e7b1daa308e1526a164dcea8ba498
celinechoo8 <celine.choo8@gmail.com>
2021-06-16 18:27:22 +1000

    added blog 59

--- src/App.js
+++ src/App.js
@@ -401,7 +401,7 @@ class App extends Component {
             <Route exact path="/resources/podcast" component={Podcast} />
             <Route path="/resources/podcast/:episode" component={EpisodePage} />
             <Route path="/resources/publications" component={Publications} />
-            {Array.from({ length: 58 }, (_, index) => index + 1).map(
+            {Array.from({ length: 59 }, (_, index) => index + 1).map(
               (blogNo) => {
                 return (
                   <Route
