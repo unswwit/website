80cedd172ecec60555fc84d73dd731abe755b916
aaalana <41637549+aaalana@users.noreply.github.com>
2021-06-20 21:17:15 +1000

    fixing tabbing

--- src/events/EventMenu.js
+++ src/events/EventMenu.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import ScrollMenu from 'react-horizontal-scrolling-menu';
-import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
-import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft';
-import UpcomingEvent from './UpcomingEvent';
+import React, { useEffect, useState } from "react";
+import ScrollMenu from "react-horizontal-scrolling-menu";
+import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
+import KeyboardArrowLeftIcon from "@material-ui/icons/KeyboardArrowLeft";
+import UpcomingEvent from "./UpcomingEvent";
 import Tabletop from "tabletop";
 import styles from "./events.module.css";
 
@@ -12,8 +12,8 @@ const MenuItem = ({ event, index }) => {
   console.log(index)
   return (
     <UpcomingEvent
-        upcomingEvent={event}
-        indes={index}
+      upcomingEvent={event}
+      index={index}
     >
     </UpcomingEvent>
   );
@@ -24,22 +24,22 @@ const MenuItem = ({ event, index }) => {
 export const Menu = (events) => events.map(el => {
   return (
     <div className={styles.gridContainer}>
-    <MenuItem
-      event={el.event}
-      index={el.index}
-      key={el.index}
-    ></MenuItem>
+      <MenuItem
+        event={el.event}
+        index={el.index}
+        key={el.index}
+      ></MenuItem>
     </div>
   );
 });
 
 const buttonStyles = {
-    backgroundColor: '#e85f5c',
-    color: "#ffffff",
-    width: "44px",
-	height: "44px",
-    borderRadius: "100%",
-    boxShadow: "1px 1px 5px 0 rgba(0, 0, 0, 0.3)",
+  backgroundColor: "#e85f5c",
+  color: "#ffffff",
+  width: "44px",
+  height: "44px",
+  borderRadius: "100%",
+  boxShadow: "1px 1px 5px 0 rgba(0, 0, 0, 0.3)",
 }
 
 const Left = ({ className }) => {
@@ -52,50 +52,51 @@ const Left = ({ className }) => {
 };
 
 const Right = ({ className }) => {
-    return (
-        <KeyboardArrowRightIcon
-        className={className}
-        style={buttonStyles}
-        ></KeyboardArrowRightIcon>
-    );
-  };
+  return (
+    <KeyboardArrowRightIcon
+      className={className}
+      style={buttonStyles}
+    ></KeyboardArrowRightIcon>
+  );
+};
 
-const ArrowLeft = Left({ className: 'arrow-prev' });
-const ArrowRight = Right({ className: 'arow-next' });
+const ArrowLeft = Left({ className: "arrow-prev" });
+const ArrowRight = Right({ className: "arrow-next" });
 
 const EventMenu = () => {
 
-    const [upcomingEvents, setUpcomingEvents] = useState([]);
-    //const [events, setEvents] = useState([]);
+  const [upcomingEvents, setUpcomingEvents] = useState([]);
+  //const [events, setEvents] = useState([]);
 
-    useEffect(() => {
-        Tabletop.init({
-            key: process.env.REACT_APP_GOOGLE_SHEETS,
-            callback: (googleData) => {
-                setUpcomingEvents(googleData["upcoming-events-test"]["elements"]);
-            },
-            simpleSheet: false,
-        });
+  useEffect(() => {
+    Tabletop.init({
+      key: process.env.REACT_APP_GOOGLE_SHEETS,
+      callback: (googleData) => {
+        setUpcomingEvents(googleData["upcoming-events-test"]["elements"]);
+      },
+      simpleSheet: false,
     });
-    
-    const events = []
-    upcomingEvents.map((upcomingEvent, index) => {
-        events.push({
-            event: upcomingEvent,
-            index: index,
-        })
-    })
 
-    // Create menu from items
-    const menu = Menu(events);
-    
-    return (
-        <ScrollMenu
-            data={menu}
-            arrowLeft={ArrowLeft}
-            arrowRight={ArrowRight}
-        />
-    );
+  }, [upcomingEvents]);
+
+  const events = []
+  upcomingEvents.forEach((upcomingEvent, index) => {
+    events.push({
+      event: upcomingEvent,
+      index: index,
+    })
+  })
+
+  // Create menu from items
+  const menu = Menu(events);
+
+  return (
+    <ScrollMenu
+      data={menu}
+      arrowLeft={ArrowLeft}
+      arrowRight={ArrowRight}
+    />
+  );
 
 }
 
