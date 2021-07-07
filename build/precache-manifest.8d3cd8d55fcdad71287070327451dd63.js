fd303afd34e841f5a388b4f439a4749eec37c116
aaalana <41637549+aaalana@users.noreply.github.com>
2021-07-07 16:38:49 +1000

    blog 63 release

--- build/precache-manifest.8d3cd8d55fcdad71287070327451dd63.js
+++ build/precache-manifest.8d3cd8d55fcdad71287070327451dd63.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "66ee936675ecf0b486ab50114ce7ffb6",
+    "revision": "ee5e3f6a93e3cd9370f0cd4ba5a0574e",
     "url": "/index.html"
   },
   {
-    "revision": "ea786a52bacc04a6a8ee",
+    "revision": "9224aeadbae1a2898772",
     "url": "/static/css/2.a38e15a8.chunk.css"
   },
   {
-    "revision": "6316190eb7b9a066a1db",
+    "revision": "4f5c64279c0b6bd47364",
     "url": "/static/css/main.18bc90c4.chunk.css"
   },
   {
-    "revision": "ea786a52bacc04a6a8ee",
-    "url": "/static/js/2.93fbf7bb.chunk.js"
+    "revision": "9224aeadbae1a2898772",
+    "url": "/static/js/2.afe82ba4.chunk.js"
   },
   {
     "revision": "9bb6834f369c0322408b15c650b08abf",
-    "url": "/static/js/2.93fbf7bb.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.afe82ba4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6316190eb7b9a066a1db",
-    "url": "/static/js/main.f438c10f.chunk.js"
+    "revision": "4f5c64279c0b6bd47364",
+    "url": "/static/js/main.7287619d.chunk.js"
   },
   {
     "revision": "3e60011f19d0b2029963",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0309cd2c1f2124434bfade5c129daec5.js"
+  "/precache-manifest.8d3cd8d55fcdad71287070327451dd63.js"
 );
 
 self.addEventListener('message', (event) => {
