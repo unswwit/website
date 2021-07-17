5baa3e252b21eb17fb373183715834a30a6127f2
Tiana Douroudis <tdouroudis@gmail.com>
2021-07-17 20:56:09 +1000

    gaps in menu

--- src/events/EventMenu.js
+++ src/events/EventMenu.js
@@ -7,30 +7,30 @@ import Tabletop from "tabletop";
 import styles from "./events.module.css";
 import "./EventMenu.css"
 
-/*
+
 // One item component
 // selected prop will be passed
-/*const MenuItem = ({ event, index }) => {
+const MenuItem = ({ event, index }) => {
   console.log(index)
   return (
     <UpcomingEvent
-      upcomingEvent={event}
+      event={event}
       index={index}
     >
     </UpcomingEvent>
   );
-};*/
+};
 
 // All items component
 // Important! add unique key
 export const Menu = (events) => events.map(el => {
   return (
     <div className={styles.menuItem}>
-      <UpcomingEvent
+      <MenuItem
         event={el.event}
         index={el.index}
-        key={el.key}
-      ></UpcomingEvent>
+        key={el.index}
+      ></MenuItem>
     </div>
   );
 });
@@ -70,28 +70,27 @@ const EventMenu = () => {
   Tabletop.init({
     key: process.env.REACT_APP_GOOGLE_SHEETS,
     callback: (googleData) => {
-      setUpcomingEvents(googleData["upcoming-events-test"]["elements"]);
+      const events = googleData["upcoming-events-test"]["elements"]
+      const eventObjects = []
+      events.forEach((event, index) => {
+        eventObjects.push({
+          event: event,
+          index: index,
+        })
+      })
+      setUpcomingEvents(eventObjects)
     },
     simpleSheet: false,
   });
     
-  const events = []
+  
 
-  upcomingEvents.forEach((upcomingEvent, index) => {
-    events.push({
-      event: upcomingEvent,
-      index: index,
-      key: upcomingEvent.img,
-    })
-  })
 
-  // Create menu from items
-  const menu = Menu(events);
   /*console.log(menu.length)*/
   return (
     <div className={styles.eventsBody}>
       <ScrollMenu
-        data={menu}
+        data={Menu(upcomingEvents)}
         arrowLeft={ArrowLeft}
         arrowRight={ArrowRight}
         wheel={false}
