cbb21af9458e07ee007fd383e72075e12e4e3765
aaalana <41637549+aaalana@users.noreply.github.com>
2021-07-18 19:04:27 +1000

    updating package.json for pagination versions

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -3,28 +3,28 @@ import { makeStyles } from "@material-ui/core/styles";
 import Pagination from "@material-ui/lab/Pagination";
 import PaginationItem from "@material-ui/lab/PaginationItem";
 
-const PaginationComp = ({ totalPages, paginate, page }) => {
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      marginBottom: theme.spacing(4),
-      padding: "2px 2px",
-      display: "flex",
-      justifyContent: "center",
-      "& .Mui-selected": {
-        backgroundColor: "#e85f5c",
-        color: "white",
-      },
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginBottom: theme.spacing(4),
+    padding: "2px 2px",
+    display: "flex",
+    justifyContent: "center",
+    "& .Mui-selected": {
+      backgroundColor: "#e85f5c",
+      color: "white",
     },
-    pagItem: {
-      backgroundColor: "transparent",
-      color: "#676767",
-      "&:hover:not(.selected)": {
-        backgroundColor: "#feb14b",
-        color: "white",
-      },
+  },
+  pagItem: {
+    backgroundColor: "transparent",
+    color: "#676767",
+    "&:hover:not(.selected)": {
+      backgroundColor: "#feb14b",
+      color: "white",
     },
-  }));
+  },
+}));
 
+const PaginationComp = ({ totalPages, paginate, page }) => {
   const classes = useStyles();
 
   return (
