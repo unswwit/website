0004da81376e3bc895fcc076aa7eb372f730e56a
joanna209 <joanna.he3@gmail.com>
2021-07-31 22:52:15 +1000

    testing endpoints

--- app.js
+++ app.js
@@ -1,80 +1,105 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const db = require('./server/db')
+// const port = 3000;
+// const execsRouter = require('./routes/execs');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-const port = 3000;
-const execsRouter = require('./routes/execs');
+const db = require('./server/db');
+const port = process.env.PORT || '3000';
 
-app.use(bodyParser.json());
+const app = express();
+app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-);
+)
 
-app.get('/', (req, res) => {
-  res.json({'message': 'ok'});
-})
-
-app.use('/execs', execsRouter);
-
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  console.error(err.message, err.stack);
-  res.status(statusCode).json({'message': err.message});
-
-
-  return;
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
 });
+
+app.get('/execs', db.getExecs);
+app.get('/subcommittee', db.getSubcommittee);
+app.get('/marketingarchives', db.getMarketingArchives); 
+
+app.listen(port, () => {
+  console.log(`The server is running at http://localhost:${port}`)
+});
+
+// app.use(bodyParser.json());
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true,
+//   })
+// );
+
+// app.get('/', (req, res) => {
+//   res.json({'message': 'ok'});
+// })
+
+// // app.use('/execs', execsRouter);
+
+// app.use((err, req, res, next) => {
+//   const statusCode = err.statusCode || 500;
+//   console.error(err.message, err.stack);
+//   res.status(statusCode).json({'message': err.message});
+
+
+//   return;
+// });
 
 // team 
 // app.get('/execs', (req, res) => {
 //   res.json({'message': 'execs'});
 // })
 
-app.get('/subcommittee', (req, res) => {
+// app.get('/subcommittee', (req, res) => {
  
-})
+// })
 
-// events 
-app.get('/past-events', (req, res) => {
+// // events 
+// app.get('/past-events', (req, res) => {
  
-})
+// })
 
-app.get('/upcoming-events', (req, res) => {
+// app.get('/upcoming-events', (req, res) => {
  
-})
+// })
 
-// sponsors
-app.get('/sponsors', (req, res) => {
+// // sponsors
+// app.get('/sponsors', (req, res) => {
  
-})
+// })
 
-// opportunities 
-app.get('/opportunities', (req, res) => {
+// // opportunities 
+// app.get('/opportunities', (req, res) => {
  
-})
+// })
 
-// resources 
-app.get('/blogs', (req, res) => {
+// // resources 
+// app.get('/blogs', (req, res) => {
  
-})
+// })
 
-app.get('/blogs/blog-authors', (req, res) => {
+// app.get('/blogs/blog-authors', (req, res) => {
  
-})
+// })
 
-app.get('/marketing-archives', (req, res) => {
+// app.get('/marketing-archives', (req, res) => {
 
-})
+// })
 
-app.get('/podcasts', (req, res) => {
+// app.get('/podcasts', (req, res) => {
 
-})
+// })
 
-app.get('/publications', (req, res) => {
+// app.get('/publications', (req, res) => {
 
-})
+// })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// });
---
+++ server/db/index.js
@@ -0,0 +1,55 @@
+const config = require('../../config');
+const helper = require('../../helper');
+const Pool = require('pg').Pool
+const pool = new Pool(config.db);
+
+// Get Execs data 
+const getExecs = (request, response) => {
+  const offset = helper.getOffset(request.query.page, config.listPerPage);
+  pool.query(
+    `SELECT email, facebook, degree, linkedin, year, position, name, img, index, year_joined 
+    FROM execs LIMIT $1 OFFSET $2`, 
+    [config.listPerPage, offset],
+    (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+// Get Subcommittee data
+const getSubcommittee = (request, response) => {
+  const offset = helper.getOffset(request.query.page, config.listPerPage);
+  pool.query(
+    `SELECT team, degree, name, year, year_joined
+    FROM subcommittee LIMIT $1 OFFSET $2`, 
+    [config.listPerPage, offset],
+    (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+// Get marketing archives data
+const getMarketingArchives = (request, response) => {
+  const offset = helper.getOffset(request.query.page, config.listPerPage);
+  pool.query(
+    `SELECT label, date, img, category, link, year
+    FROM marketing-archives LIMIT $1 OFFSET $2`, 
+    [config.listPerPage, offset],
+    (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+module.exports = {
+  getExecs,
+  getSubcommittee,
+  getMarketingArchives
+}
