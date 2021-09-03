f9b3b3934198afb04b4c2139616b3eef5e87b1d4
thefirewood <77281319+thefirewood@users.noreply.github.com>
2021-09-03 19:54:04 +0700

    adding hyphen

--- app.js
+++ app.js
@@ -18,15 +18,15 @@ app.get('/', (request, response) => {
 // Get data from database 
 app.get('/execs', db.getExecs);
 app.get('/subcommittee', db.getSubcommittee);
-app.get('/marketingarchives', db.getMarketingArchives); 
-app.get('/blogauthors', db.getBlogAuthors);
-app.get('/blogpreviews', db.getBlogPreviews);
+app.get('/marketing-archives', db.getMarketingArchives); 
+app.get('/blog-authors', db.getBlogAuthors);
+app.get('/blog-previews', db.getBlogPreviews);
 app.get('/opportunities', db.getOpportunities);
-app.get('/pastevents', db.getPastEvents);
-app.get('/podcastepisodes', db.getPodcastEpisodes);
+app.get('/past-events', db.getPastEvents);
+app.get('/podcast-episodes', db.getPodcastEpisodes);
 app.get('/publications', db.getPublications);
 app.get('/sponsors', db.getSponsors);
-app.get('/upcomingevents', db.getUpcomingEvents);
+app.get('/upcoming-events', db.getUpcomingEvents);
 
 app.listen(port, () => {
   console.log(`The server is running at http://localhost:${port}`)
