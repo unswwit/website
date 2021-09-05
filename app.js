3c74f77df094421dfdeb1cd8d74c2d1a612cf5d9
thefirewood <77281319+thefirewood@users.noreply.github.com>
2021-09-05 17:58:57 +0700

    changing '3000' to 3000

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./db');
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(bodyParser.json())
