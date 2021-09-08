bc83fc4b9b54cbf0991563b1c7329d6a8f05258e
thefirewood <77281319+thefirewood@users.noreply.github.com>
2021-09-08 21:59:14 +0700

    adding cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const db = require('./db');
 const port = process.env.PORT || 3000;
 
@@ -10,6 +11,7 @@ app.use(
     extended: true,
   })
 )
+app.use(cors());
 
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
