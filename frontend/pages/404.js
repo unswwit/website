9bcee34fac755c70f5e402b8897b578b0e305e84
joanna209 <joanna.he3@gmail.com>
2022-06-05 23:45:13 +1000

    add 404 page

---
+++ frontend/pages/404.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import Link from "next/link";
+import styles from "../styles/NotFound.module.css";
+import Image from "next/image";
+
+const NotFound = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  return (
+    <div id={styles.container}>
+      <Image
+        id={styles.image404}
+        src={`/404.png`}
+        alt="404"
+        width="350px"
+        height="350px"
+      />
+      <h2>Page Not Found</h2>
+      <p>Oops! We can't seem to find the page you are looking for.</p>
+      <div id={styles.homeBtn}>
+      <Link href="/">
+        <div className={styles.text}>Go back home</div>
+      </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
