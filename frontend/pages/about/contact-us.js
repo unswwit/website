110e13d7c6f05adde83b5be0ca21deac6e56f5ff
mrzpn <mariyapoloziuk1@gmail.com>
2022-06-15 00:30:28 +1000

    add image dimension properties

--- frontend/pages/about/contact-us.js
+++ frontend/pages/about/contact-us.js
@@ -4,6 +4,7 @@ import ContactForm from "../../components/contactForm";
 import styles from "../../styles/contactUs.module.css";
 import LoadingScreen from "../../components/LoadingScreen";
 import Image from "next/image";
+import Head from "next/head";
 
 const ContactUs = () => {
   const [sourceLoading, setSourceLoading] = useState(true);
@@ -31,6 +32,8 @@ const ContactUs = () => {
             imgUrl="/headers/2021-exec-header.jpg"
             title="Contact Us"
             imageLoading={setHeaderLoading}
+            width="3000px"
+            height="1331px"
           />
           <div className={styles.form}>
             <h2 className={styles.inquiry}>Send Through a General Inquiry</h2>
@@ -60,8 +63,8 @@ const ContactUs = () => {
                 <Image
                   src="/icons/pres-icon.png"
                   alt="pres-icon"
-                  width="45px"
-                  height="45px"
+                  width="50px"
+                  height="50px"
                 />
                 <h3>Jane Wang</h3>
                 <a href="mailto:president@unswwit.com">president@unswwit.com</a>
@@ -70,8 +73,8 @@ const ContactUs = () => {
                 <Image
                   src="/icons/pres-icon.png"
                   alt="pres-icon"
-                  width="45px"
-                  height= "45px"
+                  width="50px"
+                  height="50px"
                 />
                 <h3>Nishimi Kapoor</h3>
                 <a href="mailto:president@unswwit.com">president@unswwit.com</a>
@@ -80,8 +83,8 @@ const ContactUs = () => {
                 <Image
                   src="/icons/sec-icon.png"
                   alt="sec-icon"
-                  width="45px"
-                  height= "45px"
+                  width="50px"
+                  height="50px"
                 />
                 <h3>Emily Dang</h3>
                 <a href="mailto:secretary@unswwit.com">secretary@unswwit.com</a>
@@ -90,8 +93,8 @@ const ContactUs = () => {
                 <Image
                   src="/icons/tr-icon.png"
                   alt="tr-icon"
-                  width="45px"
-                  height= "45px"
+                  width="50px"
+                  height="50px"
                 />
                 <h3>Tiana Douroudis</h3>
                 <a href="mailto:treasurer@unswwit.com">treasurer@unswwit.com</a>
@@ -107,8 +110,8 @@ const ContactUs = () => {
                 <Image
                   src="/icons/careers-icon.png"
                   alt="careers-icon"
-                  width="45px"
-                  height= "45px"
+                  width="50px"
+                  height="50px"
                 />
                 <h3>Brianna Kim</h3>
                 <a href="mailto:careers@unswwit.com">careers@unswwit.com</a>
@@ -117,8 +120,8 @@ const ContactUs = () => {
                 <Image
                   src="/icons/careers-icon.png"
                   alt="careers-icon"
-                  width="45px"
-                  height= "45px"
+                  width="50px"
+                  height="50px"
                 />
                 <h3>Vivian Wang</h3>
                 <a href="mailto:careers@unswwit.com">careers@unswwit.com</a>
@@ -127,8 +130,8 @@ const ContactUs = () => {
                 <Image
                   src="/icons/comps-icon.png"
                   alt="comps-icon"
-                  width="45px"
-                  height= "45px"
+                  width="50px"
+                  height="50px"
                 />
                 <h3>Vivian Wong</h3>
                 <a href="mailto:competitions@unswwit.com">competitions@unswwit.com</a>
@@ -137,8 +140,8 @@ const ContactUs = () => {
                 <Image
                   src="/icons/ex-icon.png"
                   alt="ex-icon"
-                  width="45px"
-                  height= "45px"
+                  width="50px"
+                  height="50px"
                 />
                 <h3>Gayathri Balaji</h3>
                 <a href="mailto:externals@unswwit.com">externals@unswwit.com</a>
@@ -147,8 +150,8 @@ const ContactUs = () => {
                 <Image
                   src="/icons/ex-icon.png"
                   alt="ex-icon"
-                  width="45px"
-                  height= "45px"
+                  width="50px"
+                  height="50px"
                 />
                 <h3>Tanisha Dasmohaptra</h3>
                 <a href="mailto:externals@unswwit.com">externals@unswwit.com</a>
@@ -157,8 +160,8 @@ const ContactUs = () => {
                 <Image
                   src="/icons/hr-icon.png"
                   alt="hr-icon"
-                  width="45px"
-                  height= "45px"
+                  width="50px"
+                  height="50px"
                 />
                 <h3>Emily Chang</h3>
                 <a href="mailto:hr@unswwit.com">hr@unswwit.com</a>
@@ -167,8 +170,8 @@ const ContactUs = () => {
                 <Image
                   src="/icons/it.png"
                   alt="it-icon"
-                  width="45px"
-                  height= "45px"
+                  width="50px"
+                  height="50px"
                 />
                 <h3>Joanna He</h3>
                 <a href="mailto:it@unswwit.com">it@unswwit.com</a>
@@ -177,8 +180,8 @@ const ContactUs = () => {
                 <Image
                   src="/icons/mark-icon.png"
                   alt="mark-icon"
-                  width="45px"
-                  height= "45px"
+                  width="50px"
+                  height="50px"
                 />
                 <h3>Abigail Joesan Johan</h3>
                 <a href="mailto:marketing@unswwit.com">marketing@unswwit.com</a>
@@ -187,8 +190,8 @@ const ContactUs = () => {
                 <Image
                   src="/icons/mark-icon.png"
                   alt="mark-icon"
-                  width="45px"
-                  height= "45px"
+                  width="50px"
+                  height="50px"
                 />
                 <h3>Gabriella Hartanto</h3>
                 <a href="mailto:marketing@unswwit.com">marketing@unswwit.com</a>
@@ -197,8 +200,8 @@ const ContactUs = () => {
                 <Image
                   src="/icons/media-icon.png"
                   alt="media-icon"
-                  width="45px"
-                  height= "45px"
+                  width="50px"
+                  height="50px"
                 />
                 <h3>Natalie Leroy</h3>
                 <a href="mailto:media@unswwit.com">media@unswwit.com</a>
@@ -207,8 +210,8 @@ const ContactUs = () => {
                 <Image
                   src="/icons/pubs-icon.png"
                   alt="pubs-icon"
-                  width="45px"
-                  height= "45px"
+                  width="50px"
+                  height="50px"
                 />
                 <h3>Chelsea Chaffey</h3>
                 <a href="mailto:publications@unswwit.com">publications@unswwit.com</a>
@@ -217,8 +220,8 @@ const ContactUs = () => {
                 <Image
                   src="/icons/pubs-icon.png"
                   alt="pubs-icon"
-                  width="45px"
-                  height= "45px"
+                  width="50px"
+                  height="50px"
                 />
                 <h3>Yuhan Zhou</h3>
                 <a href="mailto:publications@unswwit.com">publications@unswwit.com</a>
