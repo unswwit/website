94b5c299df07bb286428d6064933a9ea9431114f
mrzpn <mariyapoloziuk1@gmail.com>
2022-06-19 18:18:12 +1000

    change hover dimensions

--- frontend/components/sponsorsModal.js
+++ frontend/components/sponsorsModal.js
@@ -22,8 +22,8 @@ const SponsorsModal = ({ handleClose, sponsorName, sponsors }) => {
           className={styles.sponsorImage}
           src={(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)?`/sponsors/2022/dark-mode/${sponsors[sponsorName][2]}`:`/sponsors/2022/${sponsors[sponsorName][2]}`}
           alt={sponsorName}
-          width="240px"
-          height="72px"
+          width="206px"
+          height="74px"
         />
         <h1 className={styles.sponsorType}>
           {sponsors[sponsorName][3]} SPONSOR
@@ -33,15 +33,6 @@ const SponsorsModal = ({ handleClose, sponsorName, sponsors }) => {
           <Link target="blank" href={sponsors[sponsorName][1]} rel="noopener noreferrer">
             <a className={styles.sponsorLink}>{sponsors[sponsorName][1]}</a>
           </Link>
-          
-          {/* <Link
-            className={styles.sponsorLink}
-            href={sponsors[sponsorName][1]}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {sponsors[sponsorName][1]}
-          </Link> */}
         </div>
         <p className={styles.sponsorDescription}>{sponsors[sponsorName][4]} </p>
       </div>
