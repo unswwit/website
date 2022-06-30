31a48851d5319d0320aad464ab2ca2aa7b222540
Edison Hien <z5308785@ad.unsw.edu.au>
2022-07-01 01:14:18 +1000

    migrate messenger customer chat to nextjs

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -2,6 +2,22 @@ import React, {useEffect} from "react";
 // import NavBar from "./Navbar";
 import Footer from "./Footer";
 import {initGA, logPageView} from "../utils/GoogleAnalytics";
+import MessengerCustomerChat from "react-messenger-customer-chat";
+
+function hideFbChat() {
+  useEffect(() => {
+    const loadingScreenExists = document.getElementById("loadingBackground");
+    const bottom =
+      Math.ceil(window.innerHeight + window.scrollY) >=
+      document.documentElement.scrollHeight;
+    console.log(bottom)
+    if (window.FB !== undefined && (!!loadingScreenExists || bottom)) {
+      window.FB.CustomerChat.hide();
+    } else if (window.FB !== undefined) {
+      window.FB.CustomerChat.show(false);
+    }
+  }, []);
+}
 
 const Layout = ({children}) => {
   useEffect(() => {
@@ -15,6 +31,11 @@ const Layout = ({children}) => {
   return <div>
     {/* <NavBar /> */}
     {children}
+    <MessengerCustomerChat
+      pageId={process.env.NEXT_PUBLIC_PAGE_ID}
+      appId={process.env.NEXT_PUBLIC_APP_ID}
+      onLoad={hideFbChat()}
+    />
     <Footer />
   </div>;
 }
