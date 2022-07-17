fe34f7dd7e1fff785134b7db6c11ac9d43d27ced
Kesh-ga <60541230+Kesh-ga@users.noreply.github.com>
2022-07-18 01:12:38 +1000

    add opportunities react files

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -21,7 +21,7 @@ const Footer = () => {
     Videos: "/media/videos",
   };
 
-  const headings = ["STUDENT INVOLVEMENT", "ABOUT US", "MEDIA"]
+  const headings = ["STUDENT INVOLVEMENT", "ABOUT US", "MEDIA"];
 
   const links = [
     ["Home", "Events", "Opportunities", "Join Us"],
@@ -58,89 +58,91 @@ const Footer = () => {
 
   return (
     <div>
-        <footer>
-          <ul className="footer-all">
-            <div className="grid-footer-container">
-              <li className="col-width footer-item footer-logo">
-                {/* TODO: change to next/image */}
-                <div className={styles.footerLogoWhite}>
-                    <Image
-                    src="/logo-white.png"
-                    alt="wit logo"
-                    width={45}
-                    height={40}
-                    />
+      <footer>
+        <ul className="footer-all">
+          <div className="grid-footer-container">
+            <li className="col-width footer-item footer-logo">
+              {/* TODO: change to next/image */}
+              <div className={styles.footerLogoWhite}>
+                <Image
+                  src="/logo-white.png"
+                  alt="wit logo"
+                  width={45}
+                  height={40}
+                />
+              </div>
+              <div className={styles.footerLogoBlack}>
+                <Image
+                  src="/logo-black.png"
+                  className="footer-logo-black"
+                  alt="wit logo"
+                  width={45}
+                  height={40}
+                />
+              </div>
+            </li>
+            {links.map((col, index) => {
+              return (
+                <div key={col} className="col-width footer-links">
+                  <li className="footer-item footer-header">
+                    {" "}
+                    {headings[index]}{" "}
+                  </li>
+                  {col.map((route) => {
+                    return (
+                      <li className="footer-item" key={route}>
+                        <Link href={routes[route] || "/"}>{route}</Link>
+                      </li>
+                    );
+                  })}
                 </div>
-                <div className={styles.footerLogoBlack}>
-                  <Image
-                    src="/logo-black.png"
-                    className="footer-logo-black"
-                    alt="wit logo"
-                    width={45}
-                    height={40}
-                  />
-                </div>
-              </li>
-              {links.map((col, index) => {
-                return (
-                  <div key={col} className="col-width footer-links">
-                    <li className="footer-item footer-header"> {headings[index]} </li>
-                    {col.map((route) => {
-                      return (
-                        <li className="footer-item" key={route}>
-                          <Link
-                            href={routes[route] || "/"}
-                          >
-                            {route}
-                          </Link>
-                        </li>
-                      );
-                    })}
-                  </div>
-                );
-              })}
-              <div className="col-width socials">
-                <div className="socials-subheading">
-                  <li className="footer-item footer-header"> FOLLOW OUR SOCIALS </li>
-                </div>
-                <div className="footer-item">
-                  <div className="square-link">
+              );
+            })}
+            <div className="col-width socials">
+              <div className="socials-subheading">
+                <li className="footer-item footer-header">
+                  {" "}
+                  FOLLOW OUR SOCIALS{" "}
+                </li>
+              </div>
+              <div className="footer-item">
+                <div className="square-link">
                   {Object.keys(socials).map((social) => {
                     return (
                       <span key={social} className="footer-icons">
                         <BootstrapTooltip
-                            key={social}
-                            title={
-                              <>
-                                <div className="tooltipTitle">{social}</div>
-                              </>
-                            }
-                          >
-                        <a
-                          href={socials[social][0]}
-                          target="_blank"
-                          rel="noopener noreferrer"
+                          key={social}
+                          title={
+                            <>
+                              <div className="tooltipTitle">{social}</div>
+                            </>
+                          }
                         >
+                          <a
+                            href={socials[social][0]}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
                             <Image
                               src={`/icons/${social}.png`}
                               alt={social}
                               width="40px"
-                              height= "40px"
+                              height="40px"
                             />
-                        </a>
+                          </a>
                         </BootstrapTooltip>
                       </span>
                     );
                   })}
-                  </div>
                 </div>
               </div>
             </div>
-          </ul>
-          <div className="footer-copyright text-center bottom-footer-item">
-            <Link href="/"> Copyright© 2020. UNSW Women in Technology </Link>
           </div>
-        </footer>
+        </ul>
+        <div className="footer-copyright text-center bottom-footer-item">
+          <Link href="/"> Copyright© 2020. UNSW Women in Technology </Link>
+        </div>
+      </footer>
     </div>
   );
 };
---
+++ frontend/pages/opportunities.js
@@ -0,0 +1,158 @@
+import React, { useEffect, useState } from "react";
+import PageHeader from "../components/header";
+import styles from "../styles/opportunities.module.css";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import LoadingScreen from "../components/LoadingScreen";
+import axios from "axios";
+import humps from "humps";
+import Link from "next/link";
+import Image from "next/image";
+
+const Opportunities = () => {
+  const [loading, setLoading] = useState(true);
+  const [opportunities, setOpportunities] = useState([]);
+  const [sourceLoading, setSourceLoading] = React.useState(true);
+  const [headerLoading, setHeaderLoading] = React.useState(true);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  const fetchOpportunities = async () => {
+    const res = await axios.get(
+      "https://wit-database.herokuapp.com/opportunities"
+    );
+    setLoading(false);
+    setOpportunities(humps.camelizeKeys(res.data));
+    setSourceLoading(false);
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    fetchOpportunities().catch((error) =>
+      // error handling
+      console.error(error)
+    );
+  }, []);
+
+  return (
+    <div>
+      {sourceLoading && headerLoading ? (
+        <LoadingScreen />
+      ) : (
+        <>
+          {/* Cover Photo */}
+          <PageHeader
+            imgUrl="/headers/opportunities-header.jfif"
+            title="Opportunities"
+            imageLoading={setHeaderLoading}
+          />
+
+          {/*start of opportunies*/}
+          <div id={styles.oppLoadingContainer}>
+            {loading && (
+              <CircularProgress
+                variant="indeterminate"
+                size={50}
+                thickness={5}
+                id={styles.oppLoading}
+              />
+            )}
+          </div>
+          {!loading &&
+            (!opportunities.length ? (
+              <div className={styles.oppLookout}>
+                <p className={styles.lookoutSize}>
+                  Keep a lookout here for upcoming opportunities!
+                </p>
+              </div>
+            ) : (
+              <div className={styles.oppBody}>
+                <p className={styles.head}>
+                  Here are all the links to all current career opportunities
+                  available ranging from internships to graduate roles. Bookmark
+                  this page and check back regularly to be the first to know
+                  about job opportunities!
+                </p>
+                <p className={styles.head}>
+                  If you would like to make a listing, please contact us
+                  at&nbsp;
+                  <a
+                    href="mailto:externals@unswwit.com"
+                    className={styles.link}
+                  >
+                    externals@unswwit.com
+                  </a>
+                  .
+                </p>
+                <div className={styles.oppGridContainer}>
+                  {opportunities.map((individualOpportunity, index) => {
+                    return (
+                      <a
+                        href={individualOpportunity.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className={styles.oppGridItems}
+                        key={index}
+                      >
+                        <div className={styles.darkOverlay} />
+                        <div className={styles.imageContainer}>
+                          {individualOpportunity.img ? (
+                            <Image
+                              className={styles.oppImg}
+                              src={
+                                process.env.PUBLIC_URL +
+                                `/sponsors/2022/${individualOpportunity.img}`
+                              }
+                              alt={individualOpportunity.companyName}
+                            />
+                          ) : (
+                            <Image
+                              className={styles.oppImg}
+                              src={
+                                process.env.PUBLIC_URL +
+                                `/opportunities/${individualOpportunity.notSponsorImg}`
+                              }
+                              alt={individualOpportunity.companyName}
+                            />
+                          )}
+                        </div>
+
+                        {/* The image name could be found in the "public/sponsors/2021" folder or add it to "public/opportunities" folder */}
+                        <div className={styles.oppDesc}>
+                          <p className={styles.oppTypeAndLocation}>
+                            {individualOpportunity.type}
+                          </p>
+                          {/* The type should be in the format of the following example: Graduate Role */}
+                          <p className={styles.jobPosition}>
+                            {individualOpportunity.position}
+                          </p>
+                          {/* The position should be in the format of the following example: Front End Developer */}
+                          <p className={styles.oppTypeAndLocation}>
+                            {individualOpportunity.location}
+                          </p>
+                          {individualOpportunity.closeDate ? (
+                            <p className={styles.oppSummary}>
+                              Applications close:{" "}
+                              {individualOpportunity.closeDate}
+                              {/* The close date should be in the format of the following example: 01/01/2021 */}
+                            </p>
+                          ) : (
+                            <p className={styles.oppSummary}></p>
+                          )}
+                          <p className={styles.oppSummary}>
+                            {individualOpportunity.summary}
+                          </p>
+                        </div>
+                      </a>
+                    );
+                  })}
+                </div>
+              </div>
+            ))}
+        </>
+      )}
+    </div>
+  );
+};
+export default Opportunities;
