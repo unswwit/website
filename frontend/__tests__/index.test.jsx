1925475428c1e04109396b5abfe4e6b2fa474c90
bruhathi31 <bruhathi.challapalli@gmail.com>
2022-07-18 16:02:42 +1000

    edit test

--- frontend/__tests__/index.test.jsx
+++ frontend/__tests__/index.test.jsx
@@ -7,7 +7,7 @@ describe('Home', () => {
     render(<Home />)
 
     const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
+      name: UNSW WOMEN IN TECHNOLOGY!/i,
     })
 
     expect(heading).toBeInTheDocument()
