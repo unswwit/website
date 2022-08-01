719ce660b7bf4ec6f423e0aacb89f05aeabedb82
Kesh-ga <60541230+Kesh-ga@users.noreply.github.com>
2022-08-01 21:09:56 +1000

    Feat/nextjs opportunities (#30)
    * add opportunities react files
    
    * add sponsors folder
    
    * update img src
    
    * change css to black to debug opp imgs
    
    * use img tags instead of image for opportunities
    
    * pr changes except for separating oppCard component
    
    * moved component
    
    * fix: opportuniies card sizing bug
    
    * returned css to normal
    
    * refactor: code style
    
    Co-authored-by: joanna209 <joanna.he3@gmail.com>

---
+++ frontend/components/OpportunitiesCard.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import styles from "../styles/opportunities.module.css";
+import Link from "next/link";
+
+class OpportunitiesCard extends Component {
+  render() {
+    return (
+      <Link href={this.props.link}>
+        <a className={styles.oppGridItems} key={this.props.index}>
+          <div className={styles.darkOverlay} />
+          <div className={styles.imageContainer}>
+            {this.props.img ? (
+              // TODO: change img tags to Image tags
+              <img
+                className={styles.oppImg}
+                src={`/sponsors/2022/${this.props.img}`}
+                alt={this.props.companyName}
+              />
+            ) : (
+              <img
+                className={styles.oppImg}
+                src={`/opportunities/${this.props.notSponsorImg}`}
+                alt={this.props.companyName}
+              />
+            )}
+          </div>
+
+          {/* The image name could be found in the "public/sponsors/2021" folder or add it to "public/opportunities" folder */}
+          <div className={styles.oppDesc}>
+            <p className={styles.oppTypeAndLocation}>{this.props.type}</p>
+            {/* The type should be in the format of the following example: Graduate Role */}
+            <p className={styles.jobPosition}>{this.props.position}</p>
+            {/* The position should be in the format of the following example: Front End Developer */}
+            <p className={styles.oppTypeAndLocation}>{this.props.location}</p>
+            {this.props.closeDate ? (
+              <p className={styles.oppSummary}>
+                Applications close: {this.props.closeDate}
+                {/* The close date should be in the format of the following example: 01/01/2021 */}
+              </p>
+            ) : (
+              <p className={styles.oppSummary}></p>
+            )}
+            <p className={styles.oppSummary}>{this.props.summary}</p>
+          </div>
+        </a>
+      </Link>
+    );
+  }
+}
+
+export default OpportunitiesCard;
