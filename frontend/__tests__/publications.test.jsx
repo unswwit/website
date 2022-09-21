c0f446f59279d5fc78f6ebea2bd45a1bac1fb85f
mrzpn <mariyapoloziuk1@gmail.com>
2022-09-21 19:10:43 +1000

    test recent publications

--- frontend/__tests__/publications.test.jsx
+++ frontend/__tests__/publications.test.jsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom'
-import { formatPublicationsDate } from "../lib/helpers";
+import { formatPublicationsDate, getRecentPublications } from "../lib/helpers";
 
 describe('Publications', () => {
     describe('formatPublicationsDate formatting', () => {
@@ -14,4 +14,37 @@ describe('Publications', () => {
       })
     })
   
-  })
+    describe('getRecentPublications', () => {
+      const publications = [
+        {
+          "id": 1,
+          "title": "test publication 1",
+          "date": "2003-11-11",
+        },
+        {
+          "id": 2,
+          "title": "test publication 2",
+          "date": "2000-11-11",
+        },
+        {
+          "id": 3,
+          "title": "test publication 3",
+          "date": "2021-10-03",
+        },
+        {
+          "id": 4,
+          "title": "test publication 4",
+          "date": "2022-10-03",
+        },
+      ]
+  
+      it('returns the 3 most recent publications by date', () => {
+        const expected = [publications[3], publications[2], publications[0]]
+        expect(getRecentPublications(publications)).toEqual(expected)
+      })
+
+      it('returns an empty array if publications dont exist', () => {
+        expect(getRecentPublications()).toEqual([])
+      })
+    })
+})
--- frontend/lib/helpers.js
+++ frontend/lib/helpers.js
@@ -7,3 +7,13 @@ export const formatPublicationsDate = (date) => {
 export const formatPodcastDate = (date) => {
   return moment(date).format("MMMM DD, YYYY");
 };
+
+export const getRecentPublications = (publications) => {
+  if (!publications) return [];
+
+  // return the 3 most recent publications (sorted by date)
+  const sortedPublications = publications.sort((a, b) => {
+    return new Date(b.date) - new Date(a.date);
+  });
+  return sortedPublications.slice(0, 3);
+};
