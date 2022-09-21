be2cae7530ebbb4c60efed24e4044c763c4840c1
Kesh-ga <60541230+Kesh-ga@users.noreply.github.com>
2022-09-21 21:07:03 +1000

    commiting api merge

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -6,56 +6,6 @@ const client = createClient({
   host: process.env.NEXT_PUBLIC_CONTENTFUL_API_HOST,
 });
 
-export async function loadPublications() {
-  const res = await client
-    .getEntries({
-      content_type: "publications",
-      select: "fields",
-      order: "-fields.index",
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-  return res.items;
-}
-
-export async function loadSubcommittee() {
-  const res = await client
-    .getEntries({
-      content_type: "subcommittee",
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-  return res.items;
-}
-
-export async function loadExecs() {
-  const res = await client
-    .getEntries({
-      content_type: "execs",
-      select: "fields",
-      order: "fields.index",
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-  return res.items;
-}
-
-export async function loadPodcasts() {
-  const res = await client
-    .getEntries({
-      content_type: "podcastEpisode",
-      select: "fields",
-      order: "-fields.episodeNo",
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-  return res.items;
-}
-
 export async function loadOpportunities() {
   const res = await client
     .getEntries({
