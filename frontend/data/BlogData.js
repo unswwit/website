f4d18a4b31daf3cbaa1cbf3670d9c513cab42f20
Kesh-ga <60541230+Kesh-ga@users.noreply.github.com>
2022-11-08 19:22:06 +1100

    image sizing resolved

--- frontend/data/BlogData.js
+++ frontend/data/BlogData.js
@@ -2,7 +2,6 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
   chip: {
-    // textTransform: "uppercase",
     color: "white",
     marginRight: "5px",
     fontFamily: "Montserrat",
