bdd471e7f550c3d0f502298bb3964c213f013395
katejoh <101443389+katejoh@users.noreply.github.com>
2022-12-29 16:53:11 +1100

    feat(frontend): update exec slideshow to use contentful (#61)
    * remove portraits and update slideshow to use contentful
    
    * style fixes

--- frontend/components/QuotesSlideshow.tsx
+++ frontend/components/QuotesSlideshow.tsx
@@ -19,144 +19,41 @@ const QuotesSlideshow = ({
   customSlideStyle,
   customQuoteStyle,
 }) => {
-  const subcoms = Object.keys(data);
-
-  const displayBackground = (key) => {
-    let quoteBlock = null;
-
-    if (data[key][1].includes('Careers')) {
-      quoteBlock = (
-        <div
-          className={[
-            styles.quoteBlock,
-            styles.quoteBlockCareers,
-            customSlideStyle,
-          ].join(' ')}
-        >
-          {displaySlideInfo(key, styles.careersRoleColour)}
-        </div>
-      );
-    } else if (data[key][1].includes('Competitions')) {
-      quoteBlock = (
-        <div
-          className={[
-            styles.quoteBlock,
-            styles.quoteBlockCompetitions,
-            customSlideStyle,
-          ].join(' ')}
-        >
-          {displaySlideInfo(key, styles.competitionsRoleColour)}
-        </div>
-      );
-    } else if (data[key][1].includes('Externals')) {
-      quoteBlock = (
-        <div
-          className={[
-            styles.quoteBlock,
-            styles.quoteBlockExternals,
-            customSlideStyle,
-          ].join(' ')}
-        >
-          {displaySlideInfo(key, styles.externalsRoleColour)}
-        </div>
-      );
-    } else if (data[key][1].includes('HR')) {
-      quoteBlock = (
-        <div
-          className={[
-            styles.quoteBlock,
-            styles.quoteBlockHR,
-            customSlideStyle,
-          ].join(' ')}
-        >
-          {displaySlideInfo(key, styles.hrRoleColour)}
-        </div>
-      );
-    } else if (data[key][1].includes('Publications')) {
-      quoteBlock = (
-        <div
-          className={[
-            styles.quoteBlock,
-            styles.quoteBlockPublications,
-            customSlideStyle,
-          ].join(' ')}
-        >
-          {displaySlideInfo(key, styles.publicationsRoleColour)}
-        </div>
-      );
-    } else if (data[key][1].includes('Marketing')) {
-      quoteBlock = (
-        <div
-          className={[
-            styles.quoteBlock,
-            styles.quoteBlockMarketing,
-            customSlideStyle,
-          ].join(' ')}
-        >
-          {displaySlideInfo(key, styles.marketingRoleColour)}
-        </div>
-      );
-    } else if (data[key][1].includes('IT')) {
-      quoteBlock = (
-        <div
-          className={[
-            styles.quoteBlock,
-            styles.quoteBlockIT,
-            customSlideStyle,
-          ].join(' ')}
-        >
-          {displaySlideInfo(key, styles.itRoleColour)}
-        </div>
-      );
-    } else if (data[key][1].includes('Media')) {
-      quoteBlock = (
-        <div
-          className={[
-            styles.quoteBlock,
-            styles.quoteBlockMedia,
-            customSlideStyle,
-          ].join(' ')}
-        >
-          {displaySlideInfo(key, styles.mediaRoleColour)}
-        </div>
-      );
-    } else {
-      quoteBlock = (
-        <div
-          className={[
-            styles.quoteBlock,
-            styles.quoteBlockDefault,
-            customSlideStyle,
-          ].join(' ')}
-        >
-          {displaySlideInfo(key, styles.defaultRoleColour)}
-        </div>
-      );
-    }
-    return quoteBlock;
+  const displayBackground = (exec) => {
+    return (
+      <div
+        className={[
+          styles.quoteBlock,
+          styles.quoteBlockDefault,
+          customSlideStyle,
+        ].join(' ')}
+      >
+        {displaySlideInfo(exec, styles.defaultRoleColour)}
+      </div>
+    );
   };
 
-  const displaySlideInfo = (key, colorClass) => {
+  const displaySlideInfo = (exec, colorClass) => {
     return (
       <>
         <div className={styles.authorBlock}>
           <Image
             className={styles.authorImage}
-            src={`/portraits/${data[key][2]}`}
-            alt={data[key][0]}
+            src={'http:' + exec.fields.image.fields.file.url}
+            alt={exec.fields.name}
             width={90}
             height={90}
           />
           <div className={styles.authorDetails}>
-            <p className={styles.authorName}>{data[key][0]}</p>
+            <p className={styles.authorName}>{exec.fields.name}</p>
             <p className={[styles.authorRoles, colorClass].join(' ')}>
-              {data[key][1]}
+              {exec.fields.position}
             </p>
           </div>
         </div>
         <div className={[styles.quoteContainer, customQuoteStyle].join(' ')}>
           <div className={styles.beginQuote}>“</div>
-          <p className={styles.quoteText}>{data[key][3]}</p>
+          <p className={styles.quoteText}>{exec.fields.quote}</p>
           <div className={styles.horizontalLine} />
           <div className={styles.endQuote}>”</div>
         </div>
@@ -165,14 +62,13 @@ const QuotesSlideshow = ({
   };
 
   // displays one slide with the given key of the dictionary
-  const displaySlide = (key) => {
+  const displaySlide = (exec, index) => {
     return (
       <Slide
-        index={key}
         innerClassName={[styles.slideContainer, customSlideStyle].join(' ')}
-        key={key}
+        key={index}
       >
-        <div className={styles.quoteBackground}>{displayBackground(key)}</div>
+        <div className={styles.quoteBackground}>{displayBackground(exec)}</div>
       </Slide>
     );
   };
@@ -182,17 +78,14 @@ const QuotesSlideshow = ({
       className={[styles.slideComponent, customSlideStyle].join(' ')}
       naturalSlideWidth={1400}
       naturalSlideHeight={height}
-      totalSlides={subcoms.length}
+      totalSlides={data.length}
     >
       <Slider
         className={styles.slideComponent}
         classNameTray={styles.slideComponent}
         classNameTrayWrap={styles.slideComponent}
       >
-        {Array.from(
-          { length: Object.keys(data).length },
-          (_, index) => index
-        ).map((slideNum) => displaySlide(+slideNum))}
+        {data.map((exec, index) => displaySlide(exec, index))}
       </Slider>
       <ButtonBack
         className={[homeLeftArrow, styles.buttonBack].join(' ')}
