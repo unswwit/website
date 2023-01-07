03bb489a810987cf3a4ddbafdeae19594689d894
joanna209 <80564580+joanna209@users.noreply.github.com>
2023-01-07 22:55:21 +1100

    feat(frontend): add google analytics

--- frontend/utils/GoogleAnalytics.ts
+++ frontend/utils/GoogleAnalytics.ts
@@ -1,7 +1,7 @@
 import ReactGA from 'react-ga';
 
 export const initGA = () => {
-  const trackingId: any = process.env.NEXT_PUBLIC_MEASUREMENT_ID; // your google analytics id
+  const trackingId: any = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
   ReactGA.initialize(trackingId);
 };
 
