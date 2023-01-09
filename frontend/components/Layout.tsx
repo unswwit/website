dfca999953c65a082520c355e48223c4a8c29fb0
joanna209 <80564580+joanna209@users.noreply.github.com>
2023-01-09 22:36:46 +1100

    feat(frontend): add google analytics (#66)

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,17 +1,8 @@
 import React from 'react';
 import Footer from './Footer';
 import NavigationBar from './NavigationBar';
-// import { initGA, logPageView } from '../utils/GoogleAnalytics';
 
 const Layout = ({ children }: any) => {
-  // useEffect(() => {
-  //   if (!window.GA_INITIALIZED) {
-  //     initGA();
-  //     window.GA_INITIALIZED = true;
-  //   }
-  //   logPageView();
-  // }, []);
-
   return (
     <div>
       <NavigationBar />
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,10 +1,25 @@
 import '../styles/globals.css';
 import Layout from '../components/Layout';
 import { AppProps } from 'next/app';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Script
+        strategy="lazyOnload"
+        src={'https://www.googletagmanager.com/gtag/js?id=G-6QEYGEF8Q5'}
+      />
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-6QEYGEF8Q5', {
+                page_path: window.location.pathname,
+              });
+                  `}
+      </Script>
       <Component {...pageProps} />
     </Layout>
   );
