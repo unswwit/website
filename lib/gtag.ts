1593cd914fb5d19ca85591ce390dd84ac8102324
joanna209 <80564580+joanna209@users.noreply.github.com>
2023-05-31 02:44:33 +1000

    fix: resolve gtag config type error

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -2,7 +2,7 @@ export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL) => {
-  window.gtag('config', GA_TRACKING_ID, {
+  window.gtag('config', GA_TRACKING_ID as string, {
     page_path: url,
   });
 };
