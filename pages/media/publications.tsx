9880a363a7cdf177feea9890d3aaed2dd94aa561
gracexkan <80564637+gracexkan@users.noreply.github.com>
2024-02-04 22:03:19 +1100

    Update publications.tsx for 2024

--- pages/media/publications.tsx
+++ pages/media/publications.tsx
@@ -62,7 +62,7 @@ const Publications = ({ publications }: any) => {
             </div>
             {/*Articles*/}
             {!loading &&
-              Array.from({ length: 4 }, (_, i) => i + 2020)
+              Array.from({ length: 5 }, (_, i) => i + 2020)
                 .reverse()
                 .map((year) => {
                   return (
