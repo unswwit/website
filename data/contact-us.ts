a1469a406f58a2ed33b746b496c62c59b45e549c
redTomatoess <159455273+redTomatoess@users.noreply.github.com>
2025-02-09 12:56:49 +1100

    Update contact-us.ts

--- data/contact-us.ts
+++ data/contact-us.ts
@@ -1,49 +1,49 @@
 // data for WIT committee profiles
 const profiles = [
     {
-        name: 'Angela Joyce Chacko',
+        name: 'Tina Dang',
         src: '/icons/pres-icon.png',
         alt: 'pres-icon',
         mail: 'president@unswwit.com',
         mailto: 'mailto:president@unswwit.com',
     }, {
-        name: 'Charlotte Fang Hendro',
+        name: 'Sehej Panesar',
         src: '/icons/pres-icon.png',
         alt: 'pres-icon',
         mail: 'president@unswwit.com',
         mailto: 'mailto:president@unswwit.com',
     }, {
-        name: 'Amy Tian',
+        name: 'Cathy Dinh',
         src: '/icons/sec-icon.png',
         alt: 'sec-icon',
         mail: 'secretary@unswwit.com',
         mailto: 'mailto:secretary@unswwit.com',
     }, {
-        name: 'Alice Shang',
+        name: 'Samanta Choo',
         src: '/icons/tr-icon.png',
         alt: 'tr-icon',
         mail: 'treasurer@unswwit.com',
         mailto: 'mailto:treasurer@unswwit.com',
     }, {
-        name: 'Crystal Zhao',
+        name: 'Nuren Alahee',
         src: '/icons/ex-icon.png',
         alt: 'ex-icon',
         mail: 'externals@unswwit.com',
         mailto: 'mailto:externals@unswwit.com',
     }, {
-        name: 'Elissa Luong',
+        name: 'Kayley Hui',
         src: '/icons/int-icon.png',
         alt: 'int-icon',
         mail: 'internals@unswwit.com',
         mailto: 'mailto:internals@unswwit.com',
     }, {
-        name: 'Grace Kan',
+        name: 'Valerie Chan',
         src: '/icons/tech.png',
         alt: 'it-icon',
         mail: 'technology@unswwit.com',
         mailto: 'mailto:technology@unswwit.com',
     }, {
-        name: 'Neysa Njo',
+        name: 'Li Yan Low',
         src: '/icons/multi-icon.png',
         alt: 'multi-icon',
         mail: 'multimedia@unswwit.com',
