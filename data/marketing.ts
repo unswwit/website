d505167d9c308bd785cbac75a52cd5449fed895d
valerie365 <159455273+valerie365@users.noreply.github.com>
2025-03-06 22:24:12 +1100

    testing2

--- data/marketing.ts
+++ data/marketing.ts
@@ -1,7 +1,7 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
-  chip: {
+  chip: { 
     textTransform: 'uppercase',
     color: 'white',
     margin: '5px',
