f234cb83f3da45657c22c7d571cfafa0552907c0
Anika <anikatahsinn0@gmail.com>
2026-02-27 22:49:33 +1100

    Anika/contact-us-page (#115)

--- data/contact-us.ts
+++ data/contact-us.ts
@@ -1,53 +1,53 @@
 // data for WIT committee profiles
 const profiles = [
     {
-        name: 'Tina Dang',
+        name: 'Trisha Chittajallu',
         src: '/icons/pres-icon.png',
         alt: 'pres-icon',
         mail: 'president@unswwit.com',
         mailto: 'mailto:president@unswwit.com',
     }, {
-        name: 'Sehej Panesar',
+        name: 'Anahita Shoree',
         src: '/icons/pres-icon.png',
         alt: 'pres-icon',
         mail: 'president@unswwit.com',
         mailto: 'mailto:president@unswwit.com',
     }, {
-        name: 'Cathy Dinh',
+        name: 'Maanya Gawande',
         src: '/icons/sec-icon.png',
         alt: 'sec-icon',
         mail: 'secretary@unswwit.com',
         mailto: 'mailto:secretary@unswwit.com',
     }, {
-        name: 'Samanta Choo',
+        name: 'Karen Hsu',
         src: '/icons/tr-icon.png',
         alt: 'tr-icon',
         mail: 'treasurer@unswwit.com',
         mailto: 'mailto:treasurer@unswwit.com',
     }, {
-        name: 'Nuren Alahee',
+        name: 'Aazeen Khan',
         src: '/icons/ex-icon.png',
         alt: 'ex-icon',
         mail: 'externals@unswwit.com',
         mailto: 'mailto:externals@unswwit.com',
     }, {
-        name: 'Kayley Hui',
+        name: 'Sophia Nguyen',
         src: '/icons/int-icon.png',
         alt: 'int-icon',
         mail: 'internals@unswwit.com',
         mailto: 'mailto:internals@unswwit.com',
     }, {
-        name: 'Valerie Chan',
+        name: 'Lily Tang',
         src: '/icons/tech.png',
         alt: 'it-icon',
         mail: 'technology@unswwit.com',
         mailto: 'mailto:technology@unswwit.com',
     }, {
-        name: 'Li Yan Low',
+        name: 'Casey Halaguena',
         src: '/icons/multi-icon.png',
         alt: 'multi-icon',
         mail: 'multimedia@unswwit.com',
         mailto: 'mailto:multimedia@unswwit.com',
-    },];
+    },]; 
 
 export default profiles;
