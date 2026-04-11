e0354f54dc54b33d0e68ec402710395518b3d762
lily <165260269+ltangg2@users.noreply.github.com>
2026-04-12 01:20:05 +1000

    Updated marketing.ts with 2026

--- data/marketing.ts
+++ data/marketing.ts
@@ -21,43 +21,43 @@ const categories = {
 const marks = [
   {
     value: 100,
+    scaledValue: 2026,
+    label: '2026',
+  },
+  {
+    value: 80,
     scaledValue: 2025,
     label: '2025',
   },
   {
-    value: 80,
+    value: 60,
     scaledValue: 2024,
     label: '2024',
   },
   {
-    value: 60,
+    value: 40,
     scaledValue: 2023,
     label: '2023',
   },
   {
-    value: 40,
+    value: 20,
     scaledValue: 2022,
     label: '2022',
   },
   {
-    value: 20,
+    value: 0,
     scaledValue: 2021,
     label: '2021',
-  },
-  {
-    value: 0,
-    scaledValue: 2020,
-    label: '2020',
   },
 ];
 
 const valueToYear = {
-  0: '2020',
-  20: '2021',
-  40: '2022',
-  60: '2023',
-  80: '2024',
-  100: '2025',
+  0: '2021',
+  20: '2022',
+  40: '2023',
+  60: '2024',
+  80: '2025',
+  100: '2026',
 };
 
 export { useStyles, categories, marks, valueToYear };
