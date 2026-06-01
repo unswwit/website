12118e73a7ac47317f9ec5c490b254f27a1ee6f7
lily <165260269+ltangg2@users.noreply.github.com>
2026-06-01 19:32:10 +1000

    Update timeline

--- pages/about/our-team.tsx
+++ pages/about/our-team.tsx
@@ -211,7 +211,7 @@ const OurTeam = ({ execs, subcommittee, directors }: any) => {
           <Timeline
             margin={'50px'}
             page={'teams'}
-            step={11.1}
+            step={11.11}
             valueToYear={valueToYear}
             marks={marks}
             updateYear={handleYear}
