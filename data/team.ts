9ecb6ada4a219f8cb3c4bf0a9e8f8c562e2315ee
lily <165260269+ltangg2@users.noreply.github.com>
2026-06-01 21:19:44 +1000

    Update team.ts

--- data/team.ts
+++ data/team.ts
@@ -12,41 +12,46 @@ const execToClassName = {
 const marks = [
   {
     value: 100,
+    scaledValue: 2026,
+    label: '2026',
+  },
+  {
+    value: 88.89,
     scaledValue: 2025,
     label: '2025',
   },
   {
-    value: 87.5,
+    value: 77.78,
     scaledValue: 2024,
     label: '2024',
   },
   {
-    value: 75,
+    value: 66.67,
     scaledValue: 2023,
     label: '2023',
   },
   {
-    value: 62.5,
+    value: 55.56,
     scaledValue: 2022,
     label: '2022',
   },
   {
-    value: 50,
+    value: 44.44,
     scaledValue: 2021,
     label: '2021',
   },
   {
-    value: 37.5,
+    value: 33.33,
     scaledValue: 2020,
     label: '2020',
   },
   {
-    value: 25,
+    value: 22.22,
     scaledValue: 2019,
     label: '2019',
   },
   {
-    value: 12.5,
+    value: 11.11,
     scaledValue: 2018,
     label: '2018',
   },
@@ -59,14 +64,15 @@ const marks = [
 
 const valueToYear = {
   0: 2017,
-  12.5: 2018,
-  25: 2019,
-  37.5: 2020,
-  50: 2021,
-  62.5: 2022,
-  75: 2023,
-  87.5: 2024,
-  100: 2025,
+  11.11: 2018,
+  22.22: 2019,
+  33.33: 2020,
+  44.44: 2021,
+  55.56: 2022,
+  66.67: 2023,
+  77.78: 2024,
+  88.89: 2025,
+  100: 2026,
 };
 
 export { execToClassName, marks, valueToYear };
