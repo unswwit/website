83c772e0da1a2ab16a2735c813597a1148db406d
shenanikans <anikatahsinn0@gmail.com>
2026-08-16 12:17:40 +1000

    added PWC sponsor info page

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -106,10 +106,12 @@ const NavigationBar = () => {
       <div className={styles.navBarGrid}>
         <div className={styles.logoContainer}>
           <Link href="/">
-            {/* change WIT logo depending on device theme and scroll position */}
+            {/* change WIT logo depending on device theme and scroll position;
+                Careers Hub pages sit on a light background so they use the
+                black logo instead of the white one used everywhere else */}
             <Image
               className={styles.logoGridItem}
-              src="/logo-white.png"
+              src={router.pathname.startsWith('/careers-hub') ? '/logo-black.png' : '/logo-white.png'}
               alt="wit logo"
               width={40}
               height={35}
